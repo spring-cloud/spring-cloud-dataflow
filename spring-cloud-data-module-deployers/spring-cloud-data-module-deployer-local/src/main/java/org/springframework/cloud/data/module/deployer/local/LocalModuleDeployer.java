028c1ac04ada20f4434411ce8021f36f4bdc9b4e
Mark Fisher <mfisher@pivotal.io>
2015-07-22 16:54:14 -0400

    initial commit for LocalModuleDeployer

---
+++ spring-cloud-data-module-deployers/spring-cloud-data-module-deployer-local/src/main/java/org/springframework/cloud/data/module/deployer/local/LocalModuleDeployer.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.data.module.deployer.local;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.data.core.ModuleDeploymentId;
+import org.springframework.cloud.data.core.ModuleDeploymentRequest;
+import org.springframework.cloud.data.module.ModuleStatus;
+import org.springframework.cloud.data.module.deployer.ModuleDeployer;
+import org.springframework.cloud.stream.module.launcher.ModuleLauncher;
+
+/**
+ * @author Mark Fisher
+ */
+public class LocalModuleDeployer implements ModuleDeployer {
+
+	private static final Logger logger = LoggerFactory.getLogger(LocalModuleDeployer.class);
+
+	private final ModuleLauncher launcher = new ModuleLauncher();
+
+	@Override
+	public ModuleDeploymentId deploy(ModuleDeploymentRequest request) {
+		String module = request.getCoordinates().toString();
+		logger.info("deploying module: " + module);
+		launcher.launch(new String[] { module }, new String[0]);
+		return new ModuleDeploymentId(request.getDefinition().getGroup(), request.getDefinition().getLabel());
+	}
+
+	@Override
+	public void undeploy(ModuleDeploymentId id) {
+		throw new UnsupportedOperationException("todo");		
+	}
+
+	@Override
+	public ModuleStatus status(ModuleDeploymentId id) {
+		throw new UnsupportedOperationException("todo");
+	}
+
+	@Override
+	public Map<ModuleDeploymentId, ModuleStatus> status() {
+		throw new UnsupportedOperationException("todo");
+	}
+}
---
+++ spring-cloud-data-module-deployers/spring-cloud-data-module-deployer-local/src/test/java/org/springframework/cloud/data/module/deployer/local/LocalModuleDeployerTests.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.data.module.deployer.local;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.cloud.data.core.ModuleCoordinates;
+import org.springframework.cloud.data.core.ModuleDefinition;
+import org.springframework.cloud.data.core.ModuleDeploymentRequest;
+
+/**
+ * Tests deployment of the time-source and log-sink modules.
+ *
+ * @author Mark Fisher
+ */
+public class LocalModuleDeployerTests {
+
+	private static final String GROUP_ID = "org.springframework.cloud.stream.module";
+
+	private static final String VERSION = "1.0.0.BUILD-SNAPSHOT";
+
+	@Test @Ignore // see TODO below
+	public void timeToLogStream() {
+		LocalModuleDeployer deployer = new LocalModuleDeployer();
+		ModuleDefinition timeDefinition = new ModuleDefinition.Builder()
+				.setGroup("ticktock")
+				.setName("time")
+				.setIndex(0)
+				.setBinding("output", "ticktock.0")
+				.build();
+		ModuleDefinition logDefinition = new ModuleDefinition.Builder()
+				.setGroup("ticktock")
+				.setName("log")
+				.setIndex(1)
+				.setBinding("input", "ticktock.0")
+				.build();
+		ModuleCoordinates timeCoordinates = new ModuleCoordinates.Builder()
+				.setGroupId(GROUP_ID)
+				.setArtifactId("time-source")
+				.setVersion(VERSION)
+				.setClassifier("exec")
+				.build();
+		ModuleCoordinates logCoordinates = new ModuleCoordinates.Builder()
+				.setGroupId(GROUP_ID)
+				.setArtifactId("log-sink")
+				.setVersion(VERSION)
+				.setClassifier("exec")
+				.build();
+		ModuleDeploymentRequest time = new ModuleDeploymentRequest(timeDefinition, timeCoordinates);
+		ModuleDeploymentRequest log = new ModuleDeploymentRequest(logDefinition, logCoordinates);
+		deployer.deploy(time);
+		deployer.deploy(log);
+		// TODO: check status, then undeploy
+	}
+}
