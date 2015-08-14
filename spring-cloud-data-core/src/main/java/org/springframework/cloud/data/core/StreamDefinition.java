8db5af3db38b57efd0a9e1000c748d01677843de
Mark Fisher <mfisher@pivotal.io>
2015-08-14 18:52:19 -0400

    removed StreamDefinitionParser

--- spring-cloud-data-core/src/main/java/org/springframework/cloud/data/core/StreamDefinition.java
+++ spring-cloud-data-core/src/main/java/org/springframework/cloud/data/core/StreamDefinition.java
@@ -16,12 +16,19 @@
 
 package org.springframework.cloud.data.core;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.springframework.cloud.data.core.parser.StreamDefinitionParser;
+import org.springframework.cloud.data.core.dsl.ArgumentNode;
+import org.springframework.cloud.data.core.dsl.ModuleNode;
+import org.springframework.cloud.data.core.dsl.SinkChannelNode;
+import org.springframework.cloud.data.core.dsl.SourceChannelNode;
+import org.springframework.cloud.data.core.dsl.StreamDslParser;
+import org.springframework.cloud.data.core.dsl.StreamNode;
 import org.springframework.core.style.ToStringCreator;
 import org.springframework.util.Assert;
 
@@ -37,10 +44,23 @@ import org.springframework.util.Assert;
  * @see ModuleDefinition
  *
  * @author Patrick Peralta
+ * @author Andy Clement
+ * @author Eric Bottard
+ * @author Mark Fisher
  */
 public class StreamDefinition {
 
-	private static final StreamDefinitionParser parser = new StreamDefinitionParser();
+	/**
+	 * Default name for input channel.
+	 */
+	// TODO: evaluate where this belongs
+	public static final String INPUT_CHANNEL = "input";
+
+	/**
+	 * Default name for output channel.
+	 */
+	// TODO: evaluate where this belongs
+	public static final String OUTPUT_CHANNEL = "output";
 
 	/**
 	 * Name of stream.
@@ -70,7 +90,8 @@ public class StreamDefinition {
 		this.name = name;
 		this.dslText = dslText;
 		this.modules = new LinkedList<>();
-		for (ModuleDefinition module : parser.parse(name, dslText)) {
+		StreamNode streamNode = new StreamDslParser(name, dslText).parse();
+		for (ModuleDefinition module : buildModuleDefinitions(name, streamNode)) {
 			this.modules.addFirst(module);
 		}
 	}
@@ -124,6 +145,57 @@ public class StreamDefinition {
 				.toString();
 	}
 
+
+	/**
+	 * Build a list of ModuleDefinitions out of a parsed StreamNode.
+	 *
+	 * @param name the name of the definition unit
+	 * @param stream the AST construct representing the definition
+	 */
+	private static List<ModuleDefinition> buildModuleDefinitions(String name, StreamNode stream) {
+		Deque<ModuleDefinition.Builder> builders = new LinkedList<>();
+
+		List<ModuleNode> moduleNodes = stream.getModuleNodes();
+		for (int m = moduleNodes.size() - 1; m >= 0; m--) {
+			ModuleNode moduleNode = moduleNodes.get(m);
+			ModuleDefinition.Builder builder =
+					new ModuleDefinition.Builder()
+							.setGroup(name)
+							.setName(moduleNode.getName())
+							.setLabel(moduleNode.getLabelName());
+			if (moduleNode.hasArguments()) {
+				ArgumentNode[] arguments = moduleNode.getArguments();
+				for (ArgumentNode argument : arguments) {
+					builder.setParameter(argument.getName(), argument.getValue());
+				}
+			}
+
+			if (m > 0) {
+				builder.addBinding(INPUT_CHANNEL, String.format("%s.%d", name, m - 1));
+			}
+			if (m < moduleNodes.size() - 1) {
+				builder.addBinding(OUTPUT_CHANNEL, String.format("%s.%d", name, m));
+			}
+
+			builders.add(builder);
+		}
+
+		SourceChannelNode sourceChannel = stream.getSourceChannelNode();
+		if (sourceChannel != null) {
+			builders.getLast().addBinding(INPUT_CHANNEL, sourceChannel.getChannelName());
+		}
+
+		SinkChannelNode sinkChannel = stream.getSinkChannelNode();
+		if (sinkChannel != null) {
+			builders.getFirst().addBinding(OUTPUT_CHANNEL, sinkChannel.getChannelName());
+		}
+
+		List<ModuleDefinition> result = new ArrayList<ModuleDefinition>(builders.size());
+		for (ModuleDefinition.Builder builder : builders) {
+			result.add(builder.build());
+		}
+		return result;
+	}
 
 	/**
 	 * Iterator that prevents mutation of its backing data structure.
--- spring-cloud-data-core/src/test/java/org/springframework/cloud/data/core/StreamDefinitionTests.java
+++ spring-cloud-data-core/src/test/java/org/springframework/cloud/data/core/StreamDefinitionTests.java
@@ -16,14 +16,22 @@
 
 package org.springframework.cloud.data.core;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
-
-import org.springframework.cloud.data.core.parser.StreamDefinitionParser;
+import org.springframework.cloud.data.core.dsl.ParseException;
 
 /**
+ * @author Mark Fisher
+ * @author David Turanski
  * @author Patrick Peralta
  */
 public class StreamDefinitionTests {
@@ -35,14 +43,180 @@ public class StreamDefinitionTests {
 		ModuleDefinition time = stream.getModuleDefinitions().get(0);
 		assertEquals("time", time.getName());
 		assertEquals("time", time.getLabel());
-		assertEquals("ticktock.0", time.getBindings().get(StreamDefinitionParser.OUTPUT_CHANNEL));
-		assertFalse(time.getBindings().containsKey(StreamDefinitionParser.INPUT_CHANNEL));
+		assertEquals("ticktock.0", time.getBindings().get(StreamDefinition.OUTPUT_CHANNEL));
+		assertFalse(time.getBindings().containsKey(StreamDefinition.INPUT_CHANNEL));
 
 		ModuleDefinition log = stream.getModuleDefinitions().get(1);
 		assertEquals("log", log.getName());
 		assertEquals("log", log.getLabel());
-		assertEquals("ticktock.0", log.getBindings().get(StreamDefinitionParser.INPUT_CHANNEL));
-		assertFalse(log.getBindings().containsKey(StreamDefinitionParser.OUTPUT_CHANNEL));
+		assertEquals("ticktock.0", log.getBindings().get(StreamDefinition.INPUT_CHANNEL));
+		assertFalse(log.getBindings().containsKey(StreamDefinition.OUTPUT_CHANNEL));
+	}
+
+	@Test
+	public void simpleStream() {
+		StreamDefinition streamDefinition = new StreamDefinition("test", "foo | bar");
+		List<ModuleDefinition> requests = streamDefinition.getModuleDefinitions(); 
+		assertEquals(2, requests.size());
+		ModuleDefinition source = requests.get(0);
+		ModuleDefinition sink = requests.get(1);
+		assertEquals("foo", source.getName());
+		assertEquals("test", source.getGroup());
+
+		assertEquals(0, source.getParameters().size());
+		assertEquals("bar", sink.getName());
+		assertEquals("test", sink.getGroup());
+		assertEquals(0, sink.getParameters().size());
+	}
+
+	@Test
+	public void quotesInParams() {
+		StreamDefinition streamDefinition = new StreamDefinition("test", "foo --bar='payload.matches(''hello'')' | file");
+		List<ModuleDefinition> requests = streamDefinition.getModuleDefinitions();
+		assertEquals(2, requests.size());
+		ModuleDefinition source = requests.get(0);
+		assertEquals("foo", source.getName());
+		assertEquals("test", source.getGroup());
+		Map<String, String> sourceParameters = source.getParameters();
+		assertEquals(1, sourceParameters.size());
+		assertEquals("payload.matches('hello')", sourceParameters.get("bar"));
+	}
+
+	@Test
+	public void quotesInParams2() {
+		StreamDefinition streamDefinition = new StreamDefinition("test",
+				"http --port=9700 | filter --expression=payload.matches('hello world') | file");
+		List<ModuleDefinition> requests = streamDefinition.getModuleDefinitions();
+		assertEquals(3, requests.size());
+		ModuleDefinition filter = requests.get(1);
+		assertEquals("filter", filter.getName());
+		assertEquals("test", filter.getGroup());
+		Map<String, String> sourceParameters = filter.getParameters();
+		assertEquals(1, sourceParameters.size());
+		assertEquals("payload.matches('hello world')", sourceParameters.get("expression"));
+	}
+
+	@Test
+	public void parameterizedModules() {
+		StreamDefinition streamDefinition = new StreamDefinition("test", "foo --x=1 --y=two | bar --z=3");
+		List<ModuleDefinition> requests = streamDefinition.getModuleDefinitions();
+		assertEquals(2, requests.size());
+		ModuleDefinition source = requests.get(0);
+		ModuleDefinition sink = requests.get(1);
+		assertEquals("foo", source.getName());
+		assertEquals("test", source.getGroup());
+		Map<String, String> sourceParameters = source.getParameters();
+		assertEquals(2, sourceParameters.size());
+		assertEquals("1", sourceParameters.get("x"));
+		assertEquals("two", sourceParameters.get("y"));
+		assertEquals("bar", sink.getName());
+		assertEquals("test", sink.getGroup());
+		Map<String, String> sinkParameters = sink.getParameters();
+		assertEquals(1, sinkParameters.size());
+		assertEquals("3", sinkParameters.get("z"));
+	}
+
+	@Test
+	public void sourceChannelNameIsAppliedToSourceModule() throws Exception {
+		StreamDefinition streamDefinition = new StreamDefinition("test", "topic:foo > goo | blah | file");
+		List<ModuleDefinition> requests = streamDefinition.getModuleDefinitions();
+		assertEquals(3, requests.size());
+		assertEquals("topic:foo", requests.get(0).getBindings().get(StreamDefinition.INPUT_CHANNEL));
+	}
+
+	@Test
+	public void sinkChannelNameIsAppliedToSinkModule() throws Exception {
+		StreamDefinition streamDefinition = new StreamDefinition("test", "boo | blah | aaak > queue:foo");
+		List<ModuleDefinition> requests = streamDefinition.getModuleDefinitions();
+		assertEquals(3, requests.size());
+		assertEquals("queue:foo", requests.get(2).getBindings().get(StreamDefinition.OUTPUT_CHANNEL));
+	}
+
+	@Test
+	public void simpleSinkNamedChannel() throws Exception {
+		StreamDefinition streamDefinition = new StreamDefinition("test", "bart > queue:foo");
+		List<ModuleDefinition> requests = streamDefinition.getModuleDefinitions();
+		assertEquals(1, requests.size());
+		assertEquals("queue:foo", requests.get(0).getBindings().get(StreamDefinition.OUTPUT_CHANNEL));
+	}
+
+	@Test
+	public void simpleSinkNamedChannelBadType() throws Exception {
+		// The parser will identify this as a Named channel sink and thus badLog will be
+		// labeled a source.
+		// But badLog is a sink and there should be an exception thrown by the parser.
+		boolean isException = false;
+		try {
+			new StreamDefinition("test", "badLog > :foo");
+		}
+		catch (Exception e) {
+			isException = true;
+		}
+		assertTrue(isException);
+	}
+
+	@Test
+	public void simpleSourceNamedChannel() throws Exception {
+		StreamDefinition streamDefinition = new StreamDefinition("test", "queue:foo > boot");
+		List<ModuleDefinition> requests = streamDefinition.getModuleDefinitions();
+		assertEquals(1, requests.size());
+		assertEquals("queue:foo", requests.get(0).getBindings().get(StreamDefinition.INPUT_CHANNEL));
+	}
+
+	@Test
+	public void namedChannelsForbiddenInComposedModules() {
+		try {
+			new StreamDefinition("test", "queue:foo > boot");
+		}
+		catch (ParseException expected) {
+			assertThat(expected.getMessage(),
+					containsString("A named channel is not supported in this kind of definition"));
+			assertThat(expected.getPosition(), is(0));
+		}
+		try {
+			new StreamDefinition("test", "bart | goo > queue:foo");
+		}
+		catch (ParseException expected) {
+			assertThat(expected.getMessage(),
+					containsString("A named channel is not supported in this kind of definition"));
+			assertThat(expected.getPosition(), is(13));
+		}
+	}
+
+	@Test
+	public void testBindings2Modules() {
+		StreamDefinition streamDefinition = new StreamDefinition("ticktock", "time | log");
+		List<ModuleDefinition> modules = streamDefinition.getModuleDefinitions();
+		ModuleDefinition source = modules.get(0);
+		ModuleDefinition sink = modules.get(1);
+		assertEquals("time", source.getLabel());
+		assertEquals("ticktock.0", source.getBindings().get(StreamDefinition.OUTPUT_CHANNEL));
+		assertFalse(source.getBindings().containsKey(StreamDefinition.INPUT_CHANNEL));
+		assertEquals("log", sink.getLabel());
+		assertEquals("ticktock.0", sink.getBindings().get(StreamDefinition.INPUT_CHANNEL));
+		assertFalse(sink.getBindings().containsKey(StreamDefinition.OUTPUT_CHANNEL));
+	}
+
+	@Test
+	public void testBindings3Modules() {
+		StreamDefinition streamDefinition = new StreamDefinition("ticktock", "time | filter |log");
+		List<ModuleDefinition> modules = streamDefinition.getModuleDefinitions();
+
+		ModuleDefinition source = modules.get(0);
+		ModuleDefinition processor = modules.get(1);
+		ModuleDefinition sink = modules.get(2);
+
+		assertEquals("time", source.getLabel());
+		assertEquals("ticktock.0", source.getBindings().get(StreamDefinition.OUTPUT_CHANNEL));
+		assertFalse(source.getBindings().containsKey(StreamDefinition.INPUT_CHANNEL));
+
+		assertEquals("filter", processor.getLabel());
+		assertEquals("ticktock.0", processor.getBindings().get(StreamDefinition.INPUT_CHANNEL));
+		assertEquals("ticktock.1", processor.getBindings().get(StreamDefinition.OUTPUT_CHANNEL));
+
+		assertEquals("log", sink.getLabel());
+		assertEquals("ticktock.1", sink.getBindings().get(StreamDefinition.INPUT_CHANNEL));
+		assertFalse(sink.getBindings().containsKey(StreamDefinition.OUTPUT_CHANNEL));
 	}
 
 }
