afe9abe54c9f58cc0a58935c1f26db322a21fb89
Janne Valkealahti <janne.valkealahti@gmail.com>
2015-09-02 09:46:59 +0100

    Fix license headers

--- spring-cloud-data-module-deployers/spring-cloud-data-module-deployer-yarn/src/test/java/org/springframework/cloud/data/module/deployer/yarn/YarnCloudAppStateMachineTests.java
+++ spring-cloud-data-module-deployers/spring-cloud-data-module-deployer-yarn/src/test/java/org/springframework/cloud/data/module/deployer/yarn/YarnCloudAppStateMachineTests.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -37,8 +37,6 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.statemachine.StateMachine;
-import org.springframework.statemachine.listener.StateMachineListenerAdapter;
-import org.springframework.statemachine.state.State;
 import org.springframework.statemachine.test.StateMachineTestPlan;
 import org.springframework.statemachine.test.StateMachineTestPlanBuilder;
 
