cb4b4c449daad79385ebf3af666fc065e8dd40d6
Eric Bottard <eric.bottard+github@gmail.com>
2015-09-04 17:03:41 +0200

    Remove unused imports

--- spring-cloud-data-admin/src/main/java/org/springframework/cloud/data/admin/config/AdminConfiguration.java
+++ spring-cloud-data-admin/src/main/java/org/springframework/cloud/data/admin/config/AdminConfiguration.java
@@ -26,12 +26,10 @@ import org.springframework.cloud.data.admin.repository.InMemoryStreamDefinitionR
 import org.springframework.cloud.data.admin.repository.InMemoryTaskDefinitionRepository;
 import org.springframework.cloud.data.admin.repository.StreamDefinitionRepository;
 import org.springframework.cloud.data.admin.repository.TaskDefinitionRepository;
-import org.springframework.cloud.data.module.deployer.cloudfoundry.CloudFoundryModuleDeployerConfiguration;
 import org.springframework.cloud.data.module.registry.ModuleRegistry;
 import org.springframework.cloud.data.module.registry.RedisModuleRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
