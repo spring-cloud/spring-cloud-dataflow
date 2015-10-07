4b14b396766273d60810dd27f738932b54f6f048
Patrick Peralta <pperalta@pivotal.io>
2015-10-07 11:57:57 -0400

    JavaDoc/etc for StreamController

--- spring-cloud-dataflow-admin-starter/src/main/java/org/springframework/cloud/dataflow/admin/controller/StreamController.java
+++ spring-cloud-dataflow-admin-starter/src/main/java/org/springframework/cloud/dataflow/admin/controller/StreamController.java
@@ -99,12 +99,12 @@ public class StreamController {
 	 * Create a {@code StreamController} that delegates
 	 * <ul>
 	 *     <li>CRUD operations to the provided {@link StreamDefinitionRepository}</li>
-	 *     <li>deployment operations to the provided {@link ModuleDeployer}</li>
 	 *     <li>module coordinate retrieval to the provided {@link ArtifactRegistry}</li>
+	 *     <li>deployment operations to the provided {@link ModuleDeployer}</li>
 	 * </ul>
 	 *
 	 * @param repository  the repository this controller will use for stream CRUD operations
-	 * @param registry    module registry this controller will use to look up modules
+	 * @param registry    artifact registry this controller will use to look up modules
 	 * @param deployer    the deployer this controller will use to deploy stream modules
 	 */
 	@Autowired
@@ -212,21 +212,27 @@ public class StreamController {
 	 */
 	@RequestMapping(value = "/deployments/{name}", method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
-	public void deploy(@PathVariable("name") String name, @RequestParam(required = false) String properties) throws
-			Exception {
+	public void deploy(@PathVariable("name") String name,
+			@RequestParam(required = false) String properties) throws Exception {
 		StreamDefinition stream = this.repository.findOne(name);
 		Assert.notNull(stream, String.format("no stream defined: %s", name));
 		deployStream(stream, DeploymentPropertiesUtils.parse(properties));
 	}
 
-	private void deployStream(StreamDefinition stream, Map<String, String> cumulatedDeploymentProperties) {
-		if (cumulatedDeploymentProperties == null) {
-			cumulatedDeploymentProperties = Collections.emptyMap();
+	/**
+	 * Deploy a stream as defined by its {@link StreamDefinition} and optional deployment properties.
+	 *
+	 * @param stream the stream to deploy
+	 * @param streamDeploymentProperties the deployment properties for the stream
+	 */
+	private void deployStream(StreamDefinition stream, Map<String, String> streamDeploymentProperties) {
+		if (streamDeploymentProperties == null) {
+			streamDeploymentProperties = Collections.emptyMap();
 		}
 		Iterator<ModuleDefinition> iterator = stream.getDeploymentOrderIterator();
 		int nextModuleCount = 0;
 		boolean isDownStreamModulePartitioned = false;
-		for (int i = 0; iterator.hasNext(); i++) {
+		while (iterator.hasNext()) {
 			ModuleDefinition currentModule = iterator.next();
 			ArtifactType type = determineModuleType(currentModule);
 			ArtifactRegistration registration = this.registry.find(currentModule.getName(), type);
@@ -235,8 +241,8 @@ public class StreamController {
 						"Module %s of type %s not found in registry", currentModule.getName(), type));
 			}
 			ArtifactCoordinates coordinates = registration.getCoordinates();
-			Map<String, String> moduleDeploymentProperties = getModuleDeploymentProperties(currentModule, cumulatedDeploymentProperties);
-			boolean upstreamModuleSupportsPartition = upstreamModuleHasPartitionInfo(stream, currentModule, cumulatedDeploymentProperties);
+			Map<String, String> moduleDeploymentProperties = extractModuleDeploymentProperties(currentModule, streamDeploymentProperties);
+			boolean upstreamModuleSupportsPartition = upstreamModuleHasPartitionInfo(stream, currentModule, streamDeploymentProperties);
 			// consumer module partition properties
 			if (isPartitionedConsumer(currentModule, moduleDeploymentProperties, upstreamModuleSupportsPartition)) {
 				updateConsumerPartitionProperties(moduleDeploymentProperties);
@@ -245,7 +251,7 @@ public class StreamController {
 			if (isDownStreamModulePartitioned) {
 				updateProducerPartitionProperties(moduleDeploymentProperties, nextModuleCount);
 			}
-			nextModuleCount = getNextModuleCount(moduleDeploymentProperties);
+			nextModuleCount = getModuleCount(moduleDeploymentProperties);
 			isDownStreamModulePartitioned = isPartitionedConsumer(currentModule, moduleDeploymentProperties,
 					upstreamModuleSupportsPartition);
 
@@ -284,6 +290,13 @@ public class StreamController {
 				.build();
 	}
 
+	/**
+	 * Return the {@link ArtifactType} for a {@link ModuleDefinition} in the context
+	 * of a defined stream.
+	 *
+	 * @param moduleDefinition the module for which to determine the type
+	 * @return {@link ArtifactType} for the given module
+	 */
 	private ArtifactType determineModuleType(ModuleDefinition moduleDefinition) {
 		// Parser has already taken care of source/sink named channels, etc
 		boolean hasOutput = moduleDefinition.getParameters().containsKey(BindingProperties.OUTPUT_BINDING_KEY);
@@ -302,17 +315,26 @@ public class StreamController {
 		}
 	}
 
-	private Map<String, String> getModuleDeploymentProperties(ModuleDefinition module, Map<String, String> deploymentProperties) {
+	/**
+	 * Extract and return a map of properties for a specific module within the
+	 * deployment properties of a stream.
+	 *
+	 * @param module module for which to return a map of properties
+	 * @param streamDeploymentProperties deployment properties for the stream that the module is defined in
+	 * @return map of properties for a module
+	 */
+	private Map<String, String> extractModuleDeploymentProperties(ModuleDefinition module,
+			Map<String, String> streamDeploymentProperties) {
 		Map<String, String> moduleDeploymentProperties = new HashMap<>();
 		String wildCardPrefix = "module.*.";
 		// first check for wild card prefix
-		for (Map.Entry<String, String> entry : deploymentProperties.entrySet()) {
+		for (Map.Entry<String, String> entry : streamDeploymentProperties.entrySet()) {
 			if (entry.getKey().startsWith(wildCardPrefix)) {
 				moduleDeploymentProperties.put(entry.getKey().substring(wildCardPrefix.length()), entry.getValue());
 			}
 		}
 		String modulePrefix = String.format("module.%s.", module.getLabel());
-		for (Map.Entry<String, String> entry : deploymentProperties.entrySet()) {
+		for (Map.Entry<String, String> entry : streamDeploymentProperties.entrySet()) {
 			if (entry.getKey().startsWith(modulePrefix)) {
 				moduleDeploymentProperties.put(entry.getKey().substring(modulePrefix.length()), entry.getValue());
 			}
@@ -320,14 +342,24 @@ public class StreamController {
 		return moduleDeploymentProperties;
 	}
 
+	/**
+	 * Return {@code true} if the upstream module (the module that appears before
+	 * the provided module) contains partition related properties.
+	 *
+	 * @param stream        stream for the module
+	 * @param currentModule module for which to determine if the upstream module
+	 *                      has partition properties
+	 * @param streamDeploymentProperties deployment properties for the stream
+	 * @return true if the upstream module has partition properties
+	 */
 	private boolean upstreamModuleHasPartitionInfo(StreamDefinition stream, ModuleDefinition currentModule,
-			Map<String, String> cumulatedDeploymentProperties) {
+			Map<String, String> streamDeploymentProperties) {
 		Iterator<ModuleDefinition> iterator = stream.getDeploymentOrderIterator();
 		while (iterator.hasNext()) {
 			ModuleDefinition module = iterator.next();
 			if (module.equals(currentModule) && iterator.hasNext()) {
 				ModuleDefinition prevModule = iterator.next();
-				Map<String, String> moduleDeploymentProperties = getModuleDeploymentProperties(prevModule, cumulatedDeploymentProperties);
+				Map<String, String> moduleDeploymentProperties = extractModuleDeploymentProperties(prevModule, streamDeploymentProperties);
 				return moduleDeploymentProperties.containsKey(BindingProperties.PARTITION_KEY_EXPRESSION) ||
 						moduleDeploymentProperties.containsKey(BindingProperties.PARTITION_KEY_EXTRACTOR_CLASS);
 			}
@@ -335,14 +367,31 @@ public class StreamController {
 		return false;
 	}
 
-	private boolean isPartitionedConsumer(ModuleDefinition module, Map<String, String> properties,
+	/**
+	 * Return {@code true} if the provided module is a consumer of partitioned data.
+	 * This is determined either by the deployment properties for the module
+	 * or whether the previous (upstream) module is publishing partitioned data.
+	 *
+	 * @param module module for which to determine if it is consuming partitioned data
+	 * @param moduleDeploymentProperties deployment properties for the module
+	 * @param upstreamModuleSupportsPartition if true, previous (upstream) module
+	 * in the stream publishes partitioned data
+	 * @return true if this module consumes partitioned data
+	 */
+	private boolean isPartitionedConsumer(ModuleDefinition module,
+			Map<String, String> moduleDeploymentProperties,
 			boolean upstreamModuleSupportsPartition) {
 		return upstreamModuleSupportsPartition ||
 				(module.getParameters().containsKey(BindingProperties.INPUT_BINDING_KEY) &&
-						properties.containsKey(BindingProperties.PARTITIONED_PROPERTY) &&
-						properties.get(BindingProperties.PARTITIONED_PROPERTY).equalsIgnoreCase("true"));
+						moduleDeploymentProperties.containsKey(BindingProperties.PARTITIONED_PROPERTY) &&
+						moduleDeploymentProperties.get(BindingProperties.PARTITIONED_PROPERTY).equalsIgnoreCase("true"));
 	}
 
+	/**
+	 * Add module properties for consuming partitioned data to the provided properties.
+	 *
+	 * @param properties properties to update
+	 */
 	private void updateConsumerPartitionProperties(Map<String, String> properties) {
 		properties.put(BindingProperties.INPUT_PARTITIONED, "true");
 		if (properties.containsKey(BindingProperties.COUNT_PROPERTY)) {
@@ -350,6 +399,12 @@ public class StreamController {
 		}
 	}
 
+	/**
+	 * Add module properties for producing partitioned data to the provided properties.
+	 *
+	 * @param properties properties to update
+	 * @param nextModuleCount the number of module instances for the next (downstream) module in the stream
+	 */
 	private void updateProducerPartitionProperties(Map<String, String> properties, int nextModuleCount) {
 		properties.put(BindingProperties.OUTPUT_PARTITION_COUNT, String.valueOf(nextModuleCount));
 		if (properties.containsKey(BindingProperties.PARTITION_KEY_EXPRESSION)) {
@@ -373,11 +428,23 @@ public class StreamController {
 		}
 	}
 
-	private int getNextModuleCount(Map<String, String> properties) {
+	/**
+	 * Return the module count indicated in the provided properties.
+	 *
+	 * @param properties properties for the module for which to determine the count
+	 * @return module count indicated in the provided properties;
+	 * if the properties do not contain a count a value of {@code 1} is returned
+	 */
+	private int getModuleCount(Map<String, String> properties) {
 		return (properties.containsKey(BindingProperties.COUNT_PROPERTY)) ?
 				Integer.valueOf(properties.get(BindingProperties.COUNT_PROPERTY)) : 1;
 	}
 
+	/**
+	 * Undeploy the given stream.
+	 *
+	 * @param stream stream to undeploy
+	 */
 	private void undeployStream(StreamDefinition stream) {
 		for (ModuleDefinition module : stream.getModuleDefinitions()) {
 			ModuleDeploymentId id = ModuleDeploymentId.fromModuleDefinition(module);
@@ -389,6 +456,13 @@ public class StreamController {
 		}
 	}
 
+	/**
+	 * Return a string that describes the state of the given stream.
+	 *
+	 * @param name name of stream to determine state for
+	 * @return stream state
+	 * @see org.springframework.cloud.dataflow.module.ModuleStatus.State
+	 */
 	private String calculateStreamState(String name) {
 		Set<ModuleStatus.State> moduleStates = new HashSet<>();
 		StreamDefinition stream = repository.findOne(name);
