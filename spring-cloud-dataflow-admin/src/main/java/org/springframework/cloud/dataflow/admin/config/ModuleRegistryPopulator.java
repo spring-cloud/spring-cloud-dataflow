bfd05d9a042d7ad5247f617b85a710f7133bd34e
Eric Bottard <eric.bottard+github@gmail.com>
2015-10-20 11:46:42 +0200

    Add FTP sink

--- spring-cloud-dataflow-admin/src/main/java/org/springframework/cloud/dataflow/admin/config/ModuleRegistryPopulator.java
+++ spring-cloud-dataflow-admin/src/main/java/org/springframework/cloud/dataflow/admin/config/ModuleRegistryPopulator.java
@@ -84,6 +84,7 @@ public class ModuleRegistryPopulator {
 		populateDefault("transform", processor);
 		populateDefault("counter", sink);
 		populateDefault("file", sink);
+		populateDefault("ftp", sink);
 		populateDefault("gemfire", sink);
 		populateDefault("hdfs", sink);
 		populateDefault("log", sink);
