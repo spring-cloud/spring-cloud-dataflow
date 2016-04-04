4f5c63c6755ecf3fe607ceba1cb61525ca12947c
Glenn Renfro <grenfro@pivotal.io>
2016-04-04 13:25:09 -0400

    Updated with current year and author tag

--- spring-cloud-dataflow-rest-client/src/main/java/org/springframework/cloud/dataflow/rest/client/DataFlowOperations.java
+++ spring-cloud-dataflow-rest-client/src/main/java/org/springframework/cloud/dataflow/rest/client/DataFlowOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ package org.springframework.cloud.dataflow.rest.client;
  * Interface the REST clients implement to interact with spring-cloud-dataflow REST API.
  *
  * @author Ilayaperumal Gopinathan
+ * @author Glenn Renfro
  */
 public interface DataFlowOperations {
 
