42fb2023c912ca5994fc19db60781403179dd6e2
BoykoAlex <aboyko@pivotal.io>
2016-04-20 20:54:41 -0400

    Polishing

--- spring-cloud-dataflow-server-core/src/test/java/org/springframework/cloud/dataflow/server/controller/AggregateCounterControllerTests.java
+++ spring-cloud-dataflow-server-core/src/test/java/org/springframework/cloud/dataflow/server/controller/AggregateCounterControllerTests.java
@@ -23,7 +23,6 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
