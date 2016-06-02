1bcbefad3300524422f9e470a25aed20ca3eac73
Gunnar Hillert <ghillert@pivotal.io>
2016-06-02 12:59:35 -0400

    gh-646 Server: Change `/admin-ui` -> `/dashboard`

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/controller/UiController.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/controller/UiController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,13 +23,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * admin ui (which comes packaged as a dependency).
  *
  * @author Eric Bottard
+ * @author Gunnar Hillert
  */
 @Controller
 public class UiController {
 
-	@RequestMapping("/admin-ui")
+	@RequestMapping("/dashboard")
 	public String index() {
-		return "redirect:/admin-ui/index.html";
+		return "redirect:/dashboard/index.html";
 	}
 
 }
