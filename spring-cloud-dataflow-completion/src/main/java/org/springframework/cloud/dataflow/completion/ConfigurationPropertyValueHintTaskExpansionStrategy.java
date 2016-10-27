102a1adc851eabc0016f858e5706e7120cbba14e
Andy Clement <aclement@pivotal.io>
2016-10-27 09:09:58 -0700

    Completion for task

---
+++ spring-cloud-dataflow-completion/src/main/java/org/springframework/cloud/dataflow/completion/ConfigurationPropertyValueHintTaskExpansionStrategy.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.completion;
+
+import static org.springframework.cloud.dataflow.completion.CompletionProposal.expanding;
+
+import java.io.IOException;
+import java.net.URLClassLoader;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationmetadata.ConfigurationMetadataProperty;
+import org.springframework.boot.configurationmetadata.ValueHint;
+import org.springframework.cloud.dataflow.configuration.metadata.ApplicationConfigurationMetadataResolver;
+import org.springframework.cloud.dataflow.core.ApplicationType;
+import org.springframework.cloud.dataflow.core.TaskDefinition;
+import org.springframework.cloud.dataflow.core.dsl.CheckPointedParseException;
+import org.springframework.cloud.dataflow.core.dsl.Token;
+import org.springframework.cloud.dataflow.core.dsl.TokenKind;
+import org.springframework.cloud.dataflow.registry.AppRegistration;
+import org.springframework.cloud.dataflow.registry.AppRegistry;
+import org.springframework.core.io.Resource;
+
+/**
+ * Attempts to fill in possible values after a {@literal --foo=prefix}
+ * (syntactically valid) construct in the DSL.
+ * @author Eric Bottard
+ * @author Mark Fisher
+ * @author Andy Clement
+ */
+public class ConfigurationPropertyValueHintTaskExpansionStrategy implements TaskExpansionStrategy {
+
+	private final AppRegistry appRegistry;
+
+	private final ApplicationConfigurationMetadataResolver metadataResolver;
+
+	@Autowired
+	private ValueHintProvider[] valueHintProviders = new ValueHintProvider[0];
+
+	ConfigurationPropertyValueHintTaskExpansionStrategy(AppRegistry appRegistry,
+			ApplicationConfigurationMetadataResolver metadataResolver) {
+		this.appRegistry = appRegistry;
+		this.metadataResolver = metadataResolver;
+	}
+
+	@Override
+	public boolean addProposals(String text, TaskDefinition parseResult,
+			int detailLevel, List<CompletionProposal> collector) {
+		Set<String> propertyNames = new HashSet<>(parseResult.getProperties().keySet());
+		propertyNames.removeAll(CompletionUtils.IMPLICIT_TASK_PARAMETER_NAMES);
+		if (text.endsWith(" ") || propertyNames.isEmpty()) {
+			return false;
+		}
+
+		String propertyName = recoverPropertyName(text);
+
+		String alreadyTyped = parseResult.getProperties().get(propertyName);
+		
+		String appName = parseResult.getRegisteredAppName();
+		AppRegistration appRegistration = appRegistry.find(appName, ApplicationType.task);
+		if (appRegistration == null) {
+			// Not a valid app name, do nothing
+			return false;
+		}
+		Resource appResource = appRegistration.getResource();
+
+		CompletionProposal.Factory proposals = expanding(text);
+
+		List<ConfigurationMetadataProperty> allProps = metadataResolver.listProperties(appResource, true);
+		List<ConfigurationMetadataProperty> whiteListedProps = metadataResolver.listProperties(appResource);
+
+		URLClassLoader classLoader = null;
+		try {
+			for (ConfigurationMetadataProperty property : allProps) {
+				if (CompletionUtils.isMatchingProperty(propertyName, property, whiteListedProps)) {
+					if (classLoader == null) {
+						classLoader = metadataResolver.createAppClassLoader(appResource);
+					}
+					for (ValueHintProvider valueHintProvider : valueHintProviders) {
+						List<ValueHint> valueHints = valueHintProvider.generateValueHints(property, classLoader);
+						if (!valueHints.isEmpty() && valueHintProvider.isExclusive(property)) {
+							collector.clear();
+						}
+						for (ValueHint valueHint : valueHints) {
+							String candidate = String.valueOf(valueHint.getValue());
+							if (!candidate.equals(alreadyTyped) && candidate.startsWith(alreadyTyped)) {
+								collector.add(proposals.withSuffix(candidate.substring(alreadyTyped.length()),
+										valueHint.getShortDescription()));
+							}
+						}
+						if (!valueHints.isEmpty() && valueHintProvider.isExclusive(property)) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (classLoader != null) {
+				try {
+					classLoader.close();
+				}
+				catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+
+		return false;
+	}
+
+	// This may be the safest way to backtrack to the property name
+	// to avoid dealing with escaped space characters, etc.
+	private String recoverPropertyName(String text) {
+		try {
+			new TaskDefinition("__dummy", text + " --");
+		}
+		catch (CheckPointedParseException exception) {
+			List<Token> tokens = exception.getTokens();
+			int end = tokens.size() - 1 - 2; // -2 for skipping dangling -- and space preceding it
+			int tokenPointer = end;
+			while (!tokens.get(tokenPointer - 1).isKind(TokenKind.DOUBLE_MINUS)) {
+				tokenPointer--;
+			}
+			StringBuilder builder;
+			for (builder = new StringBuilder(); tokenPointer < end; tokenPointer++) {
+				Token t = tokens.get(tokenPointer);
+				if (t.isIdentifier()) {
+					builder.append(t.stringValue());
+				}
+				else {
+					builder.append(t.getKind().getTokenChars());
+				}
+			}
+			return builder.toString();
+		}
+		throw new AssertionError("Can't be reached");
+	}
+
+}
---
+++ spring-cloud-dataflow-completion/src/test/java/org/springframework/cloud/dataflow/completion/TaskCompletionProviderTests.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.completion;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
+import static org.junit.Assert.assertThat;
+import static org.springframework.cloud.dataflow.completion.Proposals.proposalThat;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.dataflow.configuration.metadata.ApplicationConfigurationMetadataResolver;
+import org.springframework.cloud.dataflow.configuration.metadata.BootApplicationConfigurationMetadataResolver;
+import org.springframework.cloud.dataflow.core.ApplicationType;
+import org.springframework.cloud.dataflow.registry.AppRegistration;
+import org.springframework.cloud.dataflow.registry.AppRegistry;
+import org.springframework.cloud.deployer.resource.registry.InMemoryUriRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+/**
+ * Integration tests for TaskCompletionProvider.
+ *
+ * <p>These tests work hand in hand with a custom {@link AppRegistry} and
+ * {@link ApplicationConfigurationMetadataResolver} to provide completions for a fictional
+ * set of well known apps.</p>
+ *
+ * @author Eric Bottard
+ * @author Mark Fisher
+ * @author Andy Clement
+ */
+@SuppressWarnings("unchecked")
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {CompletionConfiguration.class, TaskCompletionProviderTests.Mocks.class})
+public class TaskCompletionProviderTests {
+
+	@Autowired
+	private TaskCompletionProvider completionProvider;
+
+	@Test
+	// <TAB> => basic,plum,etc
+	public void testEmptyStartShouldProposeSourceApps() {
+		assertThat(completionProvider.complete("", 1), hasItems(
+				proposalThat(is("basic")),
+				proposalThat(is("plum"))
+		));
+		assertThat(completionProvider.complete("", 1), not(hasItems(
+				proposalThat(is("log"))
+		)));
+	}
+
+	@Test
+	// b<TAB> => basic
+	public void testUnfinishedAppNameShouldReturnCompletions() {
+		assertThat(completionProvider.complete("b", 1), hasItems(
+				proposalThat(is("basic"))
+		));
+		assertThat(completionProvider.complete("ba", 1), hasItems(
+				proposalThat(is("basic"))
+		));
+		assertThat(completionProvider.complete("pl", 1), not(hasItems(
+				proposalThat(is("basic"))
+		)));
+	}
+
+	@Test
+	// basic<TAB> => basic --foo=, etc
+	public void testValidSubStreamDefinitionShouldReturnAppOptions() {
+		System.out.println(completionProvider.complete("basic", 1));
+		assertThat(completionProvider.complete("basic ", 1), hasItems(
+				proposalThat(is("basic --expression=")),
+				proposalThat(is("basic --expresso="))
+		));
+		// Same as above, no final space
+		assertThat(completionProvider.complete("basic", 1), hasItems(
+				proposalThat(is("basic --expression=")),
+				proposalThat(is("basic --expresso="))
+		));
+	}
+
+	@Test
+	// file | filter -<TAB> => file | filter --foo,etc
+	public void testOneDashShouldReturnTwoDashes() {
+		System.out.println(completionProvider.complete("basic -", 1));
+		assertThat(completionProvider.complete("basic -", 1), hasItems(
+				proposalThat(is("basic --expression=")),
+				proposalThat(is("basic --expresso="))
+		));
+	}
+
+	@Test
+	// basic --<TAB> => basic --foo,etc
+	public void testTwoDashesShouldReturnOptions() {
+		assertThat(completionProvider.complete("basic --", 1), hasItems(
+				proposalThat(is("basic --expression=")),
+				proposalThat(is("basic --expresso="))
+		));
+	}
+
+	@Test
+	// file --p<TAB> => file --preventDuplicates=, file --pattern=
+	public void testUnfinishedOptionNameShouldComplete() {
+		assertThat(completionProvider.complete("basic --foo", 1), hasItems(
+				proposalThat(is("basic --fooble="))
+		));
+	}
+
+	@Test
+	// file | counter --name=<TAB> => nothing
+	public void testInGenericOptionValueCantProposeAnything() {
+		assertThat(completionProvider.complete("http --port=", 1), empty());
+	}
+
+	@Test
+	// plum --use-ssl=<TAB> => propose true|false
+	public void testValueHintForBooleans() {
+		assertThat(completionProvider.complete("plum --use-ssl=", 1), hasItems(
+				proposalThat(is("plum --use-ssl=true")),
+				proposalThat(is("plum --use-ssl=false"))
+		));
+	}
+
+	@Test
+	// basic --enum-value=<TAB> => propose enum values
+	public void testValueHintForEnums() {
+		assertThat(completionProvider.complete("basic --expresso=", 1), hasItems(
+				proposalThat(is("basic --expresso=SINGLE")),
+				proposalThat(is("basic --expresso=DOUBLE"))
+		));
+	}
+
+	@Test
+	public void testUnrecognizedPrefixesDontBlowUp() {
+		assertThat(completionProvider.complete("foo", 1), empty());
+		assertThat(completionProvider.complete("foo --", 1), empty());
+		assertThat(completionProvider.complete("http --notavalidoption", 1), empty());
+		assertThat(completionProvider.complete("http --notavalidoption=", 1), empty());
+		assertThat(completionProvider.complete("foo --some-option", 1), empty());
+		assertThat(completionProvider.complete("foo --some-option=", 1), empty());
+		assertThat(completionProvider.complete("foo --some-option=prefix", 1), empty());
+	}
+
+	/*
+	 * http --use-ssl=tr<TAB> => must be true or false, no need to present "...=tr --other.prop"
+	 */
+	@Test
+	public void testClosedSetValuesShouldBeExclusive() {
+		assertThat(completionProvider.complete("http --use-ssl=tr", 1), not(hasItems(
+				proposalThat(startsWith("http --port=12 --use-ssl=tr "))
+		)));
+	}
+
+	/**
+	 * A set of mocks that consider the contents of the {@literal apps/} directory as app
+	 * archives.
+	 *
+	 * @author Eric Bottard
+	 * @author Mark Fisher
+	 */
+	@Configuration
+	public static class Mocks {
+
+		private static final File ROOT = new File("src/test/resources", Mocks.class.getPackage().getName().replace('.', '/') + "/apps");
+
+		private static final FileFilter FILTER = new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.isDirectory() && pathname.getName().matches(".+-.+");
+			}
+		};
+
+		@Bean
+		public AppRegistry appRegistry() {
+			final ResourceLoader resourceLoader = new FileSystemResourceLoader();
+			return new AppRegistry(new InMemoryUriRegistry(), resourceLoader) {
+				@Override
+				public AppRegistration find(String name, ApplicationType type) {
+					String filename = name + "-" + type;
+					File file = new File(ROOT, filename);
+					if (file.exists()) {
+						return new AppRegistration(name, type, file.toURI(), resourceLoader);
+					}
+					else {
+						return null;
+					}
+				}
+
+				@Override
+				public List<AppRegistration> findAll() {
+					List<AppRegistration> result = new ArrayList<>();
+					for (File file : ROOT.listFiles(FILTER)) {
+						result.add(makeAppRegistration(file));
+					}
+					return result;
+				}
+
+				private AppRegistration makeAppRegistration(File file) {
+					String fileName = file.getName();
+					Matcher matcher = Pattern.compile("(?<name>.+)-(?<type>.+)").matcher(fileName);
+					Assert.isTrue(matcher.matches());
+					String name = matcher.group("name");
+					ApplicationType type = ApplicationType.valueOf(matcher.group("type"));
+					System.out.println("makeAppRegistration "+fileName);
+					return new AppRegistration(name, type, file.toURI(), resourceLoader);
+				}
+			};
+		}
+
+		@Bean
+		public ApplicationConfigurationMetadataResolver metadataResolver() {
+			return new BootApplicationConfigurationMetadataResolver(TaskCompletionProviderTests.class.getClassLoader());
+		}
+	}
+
+}
