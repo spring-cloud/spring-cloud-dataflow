c6b2123892045c50078668c03f7720d0e8e208fa
Gunnar Hillert <ghillert@pivotal.io>
2017-01-03 10:04:37 -1000

    gh-810 Add support for RBAC over REST-APIs
    * Ensure the Ldap Tests work with roles

--- spring-cloud-starter-dataflow-server-local/src/test/java/org/springframework/cloud/dataflow/server/local/security/LocalServerSecurityWithLdapSimpleBindTests.java
+++ spring-cloud-starter-dataflow-server-local/src/test/java/org/springframework/cloud/dataflow/server/local/security/LocalServerSecurityWithLdapSimpleBindTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,7 +78,7 @@ public class LocalServerSecurityWithLdapSimpleBindTests {
 	}
 
 	@Test
-	public void testAuthenticatedAccessToModulesEndpointSucceeds() throws Exception {
+	public void testAuthenticatedAccessToAppsEndpointSucceeds() throws Exception {
 		localDataflowResource.getMockMvc()
 				.perform(get("/apps").header("Authorization", basicAuthorizationHeader("joe", "joespassword")))
 				.andDo(print())
