aad1616b21d844437500b9b967147308d9fa8bbf
Janne Valkealahti <janne.valkealahti@gmail.com>
2017-01-12 13:20:26 +0000

    Fix task launching properties handling
    - Fixed DefaultTaskService to strip i.e. "app.timestamp" out from
      a passed properties string so that this prefix is not passed
      into an task app itself. This is how stream apps are handled.
    - Added tests to verify properties handling.
    - Added docs how properties can be used during a task launch.
    - Effectively i.e with local server launch
      task launch --name footask1 --properties "app.timestamp.foo1=bar1,app.timestamp.foo2=bar2,app.timestamp.spring.cloud.deployer.foo3=bar3"
      will add 'foo1=bar1' and 'foo2=bar2' as task app environment variables
      and 'foo3=bar3' as a deployment property.
    - Fixes #1076

--- spring-cloud-dataflow-rest-resource/src/test/java/org/springframework/cloud/dataflow/rest/job/support/DeploymentPropertiesUtilsTests.java
+++ spring-cloud-dataflow-rest-resource/src/test/java/org/springframework/cloud/dataflow/rest/job/support/DeploymentPropertiesUtilsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +17,12 @@
 package org.springframework.cloud.dataflow.rest.job.support;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 import org.junit.Test;
 import org.springframework.cloud.dataflow.rest.util.DeploymentPropertiesUtils;
@@ -34,6 +36,16 @@ import org.springframework.cloud.dataflow.rest.util.DeploymentPropertiesUtils;
 public class DeploymentPropertiesUtilsTests {
 
 	@Test
+	public void testDeploymentProperties() {
+		assertProps("app.timestamp.foo1=bar1",
+				new String[] { "app.timestamp.foo1" },
+				new String[] { "bar1" });
+		assertProps("app.timestamp.foo1=bar1,app.timestamp.foo2=bar2",
+				new String[] { "app.timestamp.foo1", "app.timestamp.foo2" },
+				new String[] { "bar1", "bar2" });
+	}
+
+	@Test
 	public void testCommandLineParamsParsing() {
 		assertArrays(new String[] { "--format=yyyy-MM-dd" }, new String[] { "--format=yyyy-MM-dd" });
 		assertArrays(new String[] { "'--format=yyyy-MM-dd HH:mm:ss.SSS'" }, new String[] { "--format=yyyy-MM-dd HH:mm:ss.SSS" });
@@ -45,6 +57,13 @@ public class DeploymentPropertiesUtilsTests {
 		assertArrays(new String[] { " --foo1=bar1 ", " --foo2=bar2 " }, new String[] { "--foo1=bar1", "--foo2=bar2" });
 		assertArrays(new String[] { "'--format=yyyy-MM-dd HH:mm:ss.SSS'", "--foo1=bar1" },
 				new String[] { "--format=yyyy-MM-dd HH:mm:ss.SSS", "--foo1=bar1" });
+	}
+
+	private static void assertProps(String left, String[] rightKeys, String[] rightValues) {
+		Map<String, String> props = DeploymentPropertiesUtils.parse(left);
+		for (int i = 0; i < rightKeys.length; i++) {
+			assertThat(props, hasEntry(rightKeys[i], rightValues[i]));
+		}
 	}
 
 	private static void assertArrays(String[] left, String[] right) {
