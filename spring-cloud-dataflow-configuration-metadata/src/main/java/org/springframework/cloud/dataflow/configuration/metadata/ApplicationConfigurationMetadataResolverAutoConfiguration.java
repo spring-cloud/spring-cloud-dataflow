a778a2a2a9bf2602f0b6feffa4478b38f7baa0f4
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2017-02-01 14:18:50 +0530

    Remove DelegatingApplicationConfigurationMetadataResolver
      - Use BootApplicationConfigurationMetadataResolver explicitly
    
    Resolves #1147

--- spring-cloud-dataflow-configuration-metadata/src/main/java/org/springframework/cloud/dataflow/configuration/metadata/ApplicationConfigurationMetadataResolverAutoConfiguration.java
+++ spring-cloud-dataflow-configuration-metadata/src/main/java/org/springframework/cloud/dataflow/configuration/metadata/ApplicationConfigurationMetadataResolverAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,6 @@ public class ApplicationConfigurationMetadataResolverAutoConfiguration {
 	@Bean
 	@ConditionalOnMissingBean(ApplicationConfigurationMetadataResolver.class)
 	public ApplicationConfigurationMetadataResolver metadataResolver() {
-		return new DelegatingApplicationConfigurationMetadataResolver(new BootApplicationConfigurationMetadataResolver());
+		return new BootApplicationConfigurationMetadataResolver();
 	}
 }
