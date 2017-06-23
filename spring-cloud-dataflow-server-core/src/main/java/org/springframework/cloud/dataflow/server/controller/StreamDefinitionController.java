662bbd0b963e9578713eada66fadce565ef5dc84
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2017-06-23 21:14:50 +0530

    Fix AppRegistry findAll(Pageable)
     - Add ability to retrieve paginated list of AppRegistration resources when Pagination is requested
     - Add test
    
    Resolves #1397

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/controller/StreamDefinitionController.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/controller/StreamDefinitionController.java
@@ -31,6 +31,7 @@ import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.cloud.dataflow.core.ApplicationType;
 import org.springframework.cloud.dataflow.core.StreamAppDefinition;
 import org.springframework.cloud.dataflow.core.StreamDefinition;
