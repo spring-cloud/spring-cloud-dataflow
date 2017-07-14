089d038b4e6628cf541e03334fb946dab9821f91
Glenn Renfro <grenfro@pivotal.io>
2017-07-14 09:33:18 -0400

    Create RestDocs for TaskExecutionController
    resolves #1550

--- spring-cloud-dataflow-docs/src/test/java/org/springframework/cloud/dataflow/server/rest/documentation/TaskExecutionsDocumentation.java
+++ spring-cloud-dataflow-docs/src/test/java/org/springframework/cloud/dataflow/server/rest/documentation/TaskExecutionsDocumentation.java
@@ -18,33 +18,43 @@ package org.springframework.cloud.dataflow.server.rest.documentation;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static org.springframework.cloud.dataflow.core.ApplicationType.task;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Documentation for the /tasks/executions endpoint.
  *
  * @author Eric Bottard
+ * @author Glenn Renfro
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TaskExecutionsDocumentation extends BaseDocumentation {
 
 	@Before
 	public void setup() throws Exception {
 		registerApp(task, "timestamp");
-		this.mockMvc.perform(
-				post("/tasks/definitions")
-						.param("name", "my-task")
-						.param("definition", "timestamp --format='yyyy MM dd'"))
-				.andExpect(status().isOk());
+		createTaskDefinition("taskA");
+		createTaskDefinition("taskB");
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		destroyTaskDefinition("taskA");
+		destroyTaskDefinition("taskB");
 		unregisterApp(task, "timestamp");
 	}
 
@@ -52,7 +62,7 @@ public class TaskExecutionsDocumentation extends BaseDocumentation {
 	public void launchTask() throws Exception {
 		this.mockMvc.perform(
 				post("/tasks/executions")
-						.param("name", "my-task")
+						.param("name", "taskA")
 						.param("properties", "app.my-task.foo=bar,deployer.my-task.something-else=3")
 						.param("arguments", "--server.port=8080,--foo=bar"))
 				.andExpect(status().isCreated())
@@ -65,4 +75,91 @@ public class TaskExecutionsDocumentation extends BaseDocumentation {
 										.description("Command line arguments to pass to the task"))));
 	}
 
+	@Test
+	public void listTaskExecutions() throws Exception {
+		documentation.dontDocument( () -> this.mockMvc.perform(
+				post("/tasks/executions")
+						.param("name", "taskB")
+						.param("properties", "app.my-task.foo=bar,deployer.my-task.something-else=3")
+						.param("arguments", "--server.port=8080,--foo=bar"))
+				.andExpect(status().isCreated()));
+
+		this.mockMvc.perform(
+				get("/tasks/executions")
+						.param("page", "0")
+						.param("size", "10"))
+				.andDo(print())
+				.andExpect(status().isOk()).andDo(this.documentationHandler.document(
+				requestParameters(
+						parameterWithName("page")
+								.description("The zero-based page number (optional)"),
+						parameterWithName("size")
+								.description("The requested page size (optional)")),
+				responseFields(
+						fieldWithPath("_embedded.taskExecutionResourceList")
+								.description("Contains a collection of Task Executions/"),
+						fieldWithPath("_links.self").description("Link to the task execution resource"),
+						fieldWithPath("page").description("Pagination properties"))));
+	}
+
+	@Test
+	public void listTaskExecutionsByName() throws Exception {
+		this.mockMvc.perform(
+				get("/tasks/executions")
+						.param("name", "taskB")
+						.param("page", "0")
+						.param("size", "10"))
+				.andDo(print())
+				.andExpect(status().isOk()).andDo(this.documentationHandler.document(
+				requestParameters(
+						parameterWithName("page")
+								.description("The zero-based page number (optional)"),
+						parameterWithName("size")
+								.description("The requested page size (optional)"),
+						parameterWithName("name")
+								.description("The name associated with the task execution")),
+				responseFields(
+						fieldWithPath("_embedded.taskExecutionResourceList")
+								.description("Contains a collection of Task Executions/"),
+						fieldWithPath("_links.self").description("Link to the task execution resource"),
+						fieldWithPath("page").description("Pagination properties"))));
+	}
+
+	@Test
+	public void launchTaskDisplayDetail() throws Exception {
+		this.mockMvc.perform(
+				get("/tasks/executions/{id}","1"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andDo(this.documentationHandler.document(
+						pathParameters(parameterWithName("id")
+								.description("The id of an existing task execution (required)"))));
+	}
+
+
+	@Test
+	public void removeTaskExecution() throws Exception {
+		this.mockMvc.perform(
+				delete("/tasks/executions/{id}", "1"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andDo(this.documentationHandler.document(
+						pathParameters(parameterWithName("id")
+								.description("The id of an existing task execution (required)"))
+				));
+	}
+
+	private void createTaskDefinition(String taskName) throws Exception{
+		documentation.dontDocument( () -> this.mockMvc.perform(
+				post("/tasks/definitions")
+						.param("name", taskName)
+						.param("definition", "timestamp --format='yyyy MM dd'"))
+				.andExpect(status().isOk()));
+	}
+
+	private void destroyTaskDefinition(String taskName) throws Exception{
+		documentation.dontDocument( () -> this.mockMvc.perform(
+				delete("/tasks/definitions/{name}", taskName))
+				.andExpect(status().isOk()));
+	}
 }
