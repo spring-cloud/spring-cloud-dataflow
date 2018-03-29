81bd90ee249cc037dc00f1dabe3997d845a43cdc
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-03-29 15:10:05 +0100

    Fix execution directory in ci pipeline
    - As things moved from root to acceptance-tests,
      need to change dir there when running build commands.
    - Relates #477

--- acceptance-tests/ci/scripts/build-project.sh
+++ acceptance-tests/ci/scripts/build-project.sh
@@ -7,9 +7,11 @@ repository=$(pwd)/distribution-repository
 buildversion=`date '+%Y-%m-%d-%H-%M-%S'`
 
 pushd git-repo > /dev/null
+pushd acceptance-tests > /dev/null
 echo $ARTIFACTORY_PASSWORD | docker login -u $ARTIFACTORY_USERNAME --password-stdin springsource-docker-private-local.jfrog.io
 ./gradlew clean build || n=1
 tar -zc --ignore-failed-read --file ${repository}/spring-cloud-skipper-acceptance-tests-${buildversion}.tar.gz spring-cloud-skipper-acceptance-tests/build/test-docker-logs
+popd > /dev/null
 popd > /dev/null
 
 if [ "$n" -gt 0 ]; then
--- acceptance-tests/ci/scripts/build-skipper-server-with-drivers101.sh
+++ acceptance-tests/ci/scripts/build-skipper-server-with-drivers101.sh
@@ -5,7 +5,9 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
+pushd acceptance-tests > /dev/null
 pushd custom-apps/skipper-server-with-drivers101 > /dev/null
 ./gradlew clean build install -Dmaven.repo.local=${repository}
+popd > /dev/null
 popd > /dev/null
 popd > /dev/null
