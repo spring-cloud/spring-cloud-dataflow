9a72da18631f20d3a244c9511b870425d3a856c9
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-05-29 16:07:27 +0100

    Remove repo add/delete commands
    - Modify shell and modify docs.
    - Fixes #653

--- spring-cloud-skipper-shell-commands/src/main/java/org/springframework/cloud/skipper/shell/command/RepositoryCommands.java
+++ spring-cloud-skipper-shell-commands/src/main/java/org/springframework/cloud/skipper/shell/command/RepositoryCommands.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2017-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,6 @@ package org.springframework.cloud.skipper.shell.command;
 
 import java.util.LinkedHashMap;
 
-import javax.validation.constraints.NotNull;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.skipper.client.SkipperClient;
 import org.springframework.cloud.skipper.domain.Repository;
@@ -26,13 +24,10 @@ import org.springframework.cloud.skipper.shell.command.support.TableUtils;
 import org.springframework.hateoas.Resources;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import org.springframework.shell.standard.ShellOption;
 import org.springframework.shell.table.BeanListTableModel;
 import org.springframework.shell.table.Table;
 import org.springframework.shell.table.TableBuilder;
 import org.springframework.shell.table.TableModel;
-
-import static org.springframework.shell.standard.ShellOption.NULL;
 
 /**
  * Commands to add, delete and list package repositories.
@@ -46,20 +41,6 @@ public class RepositoryCommands extends AbstractSkipperCommand {
 	@Autowired
 	public RepositoryCommands(SkipperClient skipperClient) {
 		this.skipperClient = skipperClient;
-	}
-
-	@ShellMethod(key = "repo add", value = "Add package repository")
-	public String add(@ShellOption(help = "name of the repository") @NotNull String name,
-			@ShellOption(help = "the root URL that points to index.yaml file") @NotNull String url,
-			@ShellOption(help = "the source URL to the package", defaultValue = NULL) String sourceUrl) {
-		Repository repository = this.skipperClient.addRepository(name, url, sourceUrl);
-		return "Repository '" + name + "' added.";
-	}
-
-	@ShellMethod(key = "repo delete", value = "Delete a package repository")
-	public String delete(@ShellOption(help = "name of the repository to delete") @NotNull String name) {
-		this.skipperClient.deleteRepository(name);
-		return "Repository '" + name + "' deleted.";
 	}
 
 	@ShellMethod(key = "repo list", value = "List package repositories")
