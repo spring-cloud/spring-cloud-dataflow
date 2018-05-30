835f6efe0f369aa2d1187ec61808391464c8cfe6
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2018-05-30 16:46:34 +0530

    Handle exception during package delete

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/stream/SkipperStreamDeployer.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/stream/SkipperStreamDeployer.java
@@ -226,7 +226,13 @@ public class SkipperStreamDeployer implements StreamDeployer {
 			release = this.skipperClient.install(installRequest);
 		}
 		catch (Exception e) {
-			this.skipperClient.packageDelete(packageName);
+			logger.error("Skipper install failed. Deleting the package: " + packageName);
+			try {
+				this.skipperClient.packageDelete(packageName);
+			}
+			catch (Exception e1) {
+				logger.error("Package delete threw exception: " + e1.getMessage());
+			}
 			throw new SkipperException(e.getMessage());
 		}
 		// TODO store releasename in deploymentIdRepository...
