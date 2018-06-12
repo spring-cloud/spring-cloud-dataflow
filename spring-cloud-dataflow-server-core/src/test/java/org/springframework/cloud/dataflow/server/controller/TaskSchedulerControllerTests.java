b9347d5b319f80e1265ccb8347ee33fa4ce159c8
Christian Tzolov <christian.tzolov@gmail.com>
2018-06-12 17:55:45 +0200

    Auto-quote CTR properties with blank characters
    Resolves #2114
    
    - for CTR tasks auto-quote the property values that contain blank or special characters.
    - Reuses same auto-quote utility used by StreamDslConverter
    - Factor out the `auto-Quotes` function in a common utility DefinitionUtils class
    - Ensure that all @SpringBootTest(classes = TestDependencies.class) tests use DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD
    - Remove the After test cleaning for DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD annotated tests

--- spring-cloud-dataflow-server-core/src/test/java/org/springframework/cloud/dataflow/server/controller/TaskSchedulerControllerTests.java
+++ spring-cloud-dataflow-server-core/src/test/java/org/springframework/cloud/dataflow/server/controller/TaskSchedulerControllerTests.java
@@ -20,7 +20,6 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,6 +34,7 @@ import org.springframework.cloud.deployer.resource.registry.UriRegistry;
 import org.springframework.cloud.scheduler.spi.core.ScheduleInfo;
 import org.springframework.cloud.scheduler.spi.core.SchedulerPropertyKeys;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -58,6 +58,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestDependencies.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class TaskSchedulerControllerTests {
 
 	@Autowired
@@ -81,13 +82,6 @@ public class TaskSchedulerControllerTests {
 	public void setupMockMVC() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
 				.defaultRequest(get("/").accept(MediaType.APPLICATION_JSON)).build();
-	}
-
-	@After
-	public void tearDown() {
-		repository.deleteAll();
-		assertEquals(0, repository.count());
-		simpleTestScheduler.getSchedules().clear();
 	}
 
 	@Test(expected = IllegalArgumentException.class)
