3f8bffcca5a6af7b8db5b6697ec3ec4be044da24
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2019-08-16 12:36:01 +0530

    Fix RootController test
     - Fix the test to check for the SCDF REST response for root path `/`
     - Updated the current root path response
    
    Resolves #3412

--- spring-cloud-dataflow-server-core/src/test/java/org/springframework/cloud/dataflow/server/controller/RootControllerTests.java
+++ spring-cloud-dataflow-server-core/src/test/java/org/springframework/cloud/dataflow/server/controller/RootControllerTests.java
@@ -20,31 +20,19 @@ import java.nio.charset.Charset;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.dataflow.core.AppRegistration;
-import org.springframework.cloud.dataflow.registry.service.AppRegistryService;
-import org.springframework.cloud.dataflow.server.config.apps.CommonApplicationProperties;
 import org.springframework.cloud.dataflow.server.configuration.TestDependencies;
-import org.springframework.cloud.dataflow.server.registry.DataFlowAppRegistryPopulator;
-import org.springframework.cloud.dataflow.server.support.MockUtils;
-import org.springframework.cloud.skipper.client.SkipperClient;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -55,47 +43,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Christian Tzolov
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { TestDependencies.class }, properties = {
-		"spring.cloud.dataflow.features.analytics-enabled=false" })
-@EnableConfigurationProperties({ CommonApplicationProperties.class })
-@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
-@Transactional
+@SpringBootTest(classes = { TestDependencies.class })
 @AutoConfigureTestDatabase(replace = Replace.ANY)
-@Ignore("Original test is wrong")
 public class RootControllerTests {
-	// Ignored for now as even origin test is testing response
-	// what a real instance would not output.
-	// With boot 2.2.x changes we get different type of response
-	// which doesn't match what a real intance would output.
-	// Test config is just wrong.
 
 	private MockMvc mockMvc;
 
 	@Autowired
 	private WebApplicationContext wac;
 
-	@Autowired
-	private AppRegistryService appRegistryService;
-
-	@MockBean
-	private SkipperClient skipperClient;
-
-	@Autowired
-	private DataFlowAppRegistryPopulator uriRegistryPopulator;
-
 	@Before
 	public void setupMocks() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
 				.defaultRequest(get("/").accept(MediaType.APPLICATION_JSON)).build();
-		for (AppRegistration appRegistration : this.appRegistryService.findAll()) {
-			this.appRegistryService.delete(appRegistration.getName(), appRegistration.getType(), appRegistration.getVersion());
-		}
-		this.uriRegistryPopulator.afterPropertiesSet();
-		this.skipperClient = MockUtils.configureMock(this.skipperClient);
 	}
 
 	@Test
-	public void testRegisterVersionedApp() throws Exception {
+	public void testRootControllerResponse() throws Exception {
 		String mvcResult = mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
 
