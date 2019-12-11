aea7d207a77ccf8570c1bfed753bc2e107f051f1
Glenn Renfro <grenfro@pivotal.io>
2019-12-11 16:33:32 -0500

    Args are stored from previous execs but not used for the next launch
    resolves #3694

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/service/impl/diff/TaskAnalyzer.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/service/impl/diff/TaskAnalyzer.java
@@ -15,7 +15,6 @@
  */
 package org.springframework.cloud.dataflow.server.service.impl.diff;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.cloud.dataflow.core.TaskManifest;
@@ -39,88 +38,25 @@ public class TaskAnalyzer {
 	 */
 	public TaskAnalysisReport analyze(TaskManifest existing, TaskManifest replacing) {
 		Map<String, String> existingDeploymentProperties = null;
-		Map<String, String> existingCommandLineArgumentPropertiesDoubleDash = new HashMap<>();
-		Map<String, String> existingCommandLineArgumentPropertiesSingleDash = new HashMap<>();
-		Map<String, String> existingCommandLineArgumentPropertiesNoDash = new HashMap<>();
 		if (existing != null) {
 			AppDeploymentRequest taskDeploymentRequest = existing.getTaskDeploymentRequest();
 			if (taskDeploymentRequest != null) {
 				existingDeploymentProperties = taskDeploymentRequest.getDeploymentProperties();
-
-				for (String arg : taskDeploymentRequest.getCommandlineArguments()) {
-					if (arg.startsWith("--")) {
-						String[] split = arg.substring(2).split("=", 2);
-						if (split.length == 2) {
-							existingCommandLineArgumentPropertiesDoubleDash.put(split[0], split[1]);
-						}
-					}
-					else if (arg.startsWith("-")) {
-						String[] split = arg.substring(1).split("=", 2);
-						if (split.length == 2) {
-							existingCommandLineArgumentPropertiesSingleDash.put(split[0], split[1]);
-						}
-					}
-					else {
-						String[] split = arg.split("=", 2);
-						if (split.length == 2) {
-							existingCommandLineArgumentPropertiesNoDash.put(split[0], split[1]);
-						}
-					}
-				}
 			}
 		}
 
 		Map<String, String> replacingDeploymentProperties = null;
-		Map<String, String> replacingCommandLineArgumentPropertiesDoubleDash = new HashMap<>();
-		Map<String, String> replacingCommandLineArgumentPropertiesSingleDash = new HashMap<>();
-		Map<String, String> replacingCommandLineArgumentPropertiesNoDash = new HashMap<>();
 		if (replacing != null) {
 			AppDeploymentRequest taskDeploymentRequest = replacing.getTaskDeploymentRequest();
 			if (taskDeploymentRequest != null) {
 				replacingDeploymentProperties = taskDeploymentRequest.getDeploymentProperties();
-
-				for (String arg : taskDeploymentRequest.getCommandlineArguments()) {
-					if (arg.startsWith("--")) {
-						String[] split = arg.substring(2).split("=", 2);
-						if (split.length == 2) {
-							replacingCommandLineArgumentPropertiesDoubleDash.put(split[0], split[1]);
-						}
-					}
-					else if (arg.startsWith("-")) {
-						String[] split = arg.substring(1).split("=", 2);
-						if (split.length == 2) {
-							replacingCommandLineArgumentPropertiesSingleDash.put(split[0], split[1]);
-						}
-					}
-					else {
-						String[] split = arg.split("=", 2);
-						if (split.length == 2) {
-							replacingCommandLineArgumentPropertiesNoDash.put(split[0], split[1]);
-						}
-					}
-				}
 			}
 		}
 
 		PropertiesDiff deploymentPropertiesDifference = PropertiesDiff.builder().left(existingDeploymentProperties)
 				.right(replacingDeploymentProperties).build();
 
-		PropertiesDiff existingCommandLineArgumentPropertiesDifferenceDoubleDash = PropertiesDiff.builder()
-				.left(existingCommandLineArgumentPropertiesDoubleDash)
-				.right(replacingCommandLineArgumentPropertiesDoubleDash).build();
-
-		PropertiesDiff existingCommandLineArgumentPropertiesDifferenceSingleDash = PropertiesDiff.builder()
-				.left(existingCommandLineArgumentPropertiesSingleDash)
-				.right(replacingCommandLineArgumentPropertiesSingleDash).build();
-
-		PropertiesDiff existingCommandLineArgumentPropertiesDifferenceNoDash = PropertiesDiff.builder()
-				.left(existingCommandLineArgumentPropertiesNoDash).right(replacingCommandLineArgumentPropertiesNoDash)
-				.build();
-
-		TaskManifestDifference taskManifestDifference = new TaskManifestDifference(deploymentPropertiesDifference,
-				existingCommandLineArgumentPropertiesDifferenceDoubleDash,
-				existingCommandLineArgumentPropertiesDifferenceSingleDash,
-				existingCommandLineArgumentPropertiesDifferenceNoDash);
+		TaskManifestDifference taskManifestDifference = new TaskManifestDifference(deploymentPropertiesDifference);
 		return new TaskAnalysisReport(taskManifestDifference);
 	}
 }
