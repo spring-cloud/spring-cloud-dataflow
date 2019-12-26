9ad9927ce2901ad689ae66b4efd6035043f3e39f
Christian Tzolov <christian.tzolov@gmail.com>
2019-12-26 17:23:03 +0100

    add  DockerComposePostgresRabbitInfluxIT to run the same IT suite but with Postgres, InfluxDB and RabbitMQ configuration

---
+++ spring-cloud-dataflow-server/src/test/java/org/springframework/cloud/dataflow/integration/test/DockerComposePostgresRabbitInfluxIT.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.dataflow.integration.test;
+
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.configuration.DockerComposeFiles;
+import com.palantir.docker.compose.connection.DockerMachine;
+import com.palantir.docker.compose.connection.waiting.HealthChecks;
+import org.junit.ClassRule;
+import org.junit.rules.ExternalResource;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import org.springframework.cloud.dataflow.integration.test.util.ClosableDockerComposeRule;
+
+import static org.springframework.cloud.dataflow.integration.test.DockerComposeIT.DATAFLOW_VERSION;
+import static org.springframework.cloud.dataflow.integration.test.DockerComposeIT.RABBITMQ_MAVEN_STREAM_APPS_URI;
+import static org.springframework.cloud.dataflow.integration.test.DockerComposeIT.SKIPPER_VERSION;
+import static org.springframework.cloud.dataflow.integration.test.DockerComposeIT.TASK_APPS_URI;
+
+/**
+ * IT Tests with Postgres, InfluxDB and RabbitMQ
+ *
+ * @author Christian Tzolov
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		DockerComposeTestPlatform.class,
+		DockerComposeTestStream.class,
+		DockerComposeTestTask.class
+})
+public class DockerComposePostgresRabbitInfluxIT {
+
+	@ClassRule
+	public static ExternalResource dockerRuleWrapper = ClosableDockerComposeRule.of(
+			DockerComposeRule.builder()
+					.files(DockerComposeFiles.from(
+							"docker-compose.yml",
+							"docker-compose-influxdb.yml",
+							"docker-compose-postgres.yml",
+							"docker-compose-rabbitmq.yml"))
+					.machine(DockerMachine.localMachine()
+							.withAdditionalEnvironmentVariable("DATAFLOW_VERSION", DATAFLOW_VERSION)
+							.withAdditionalEnvironmentVariable("SKIPPER_VERSION", SKIPPER_VERSION)
+							.withAdditionalEnvironmentVariable("STREAM_APPS_URI", RABBITMQ_MAVEN_STREAM_APPS_URI)
+							.withAdditionalEnvironmentVariable("TASK_APPS_URI", TASK_APPS_URI)
+							.build())
+					.saveLogsTo("target/dockerLogs/DockerComposePostgresRabbitInfluxIT")
+					.waitingForService("dataflow-server", HealthChecks.toRespond2xxOverHttp(9393,
+							(port) -> port.inFormat("http://$HOST:$EXTERNAL_PORT")))
+					.waitingForService("skipper-server", HealthChecks.toRespond2xxOverHttp(7577,
+							(port) -> port.inFormat("http://$HOST:$EXTERNAL_PORT")))
+					.pullOnStartup(true)
+					.build());
+}
