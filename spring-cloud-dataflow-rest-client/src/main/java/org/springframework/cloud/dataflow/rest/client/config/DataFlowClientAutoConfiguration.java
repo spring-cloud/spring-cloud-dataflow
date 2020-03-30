6b99550b068ddec1a5eb871683a82dd44d7e5732
Janne Valkealahti <janne.valkealahti@gmail.com>
2020-03-30 14:29:42 +0100

    Add bearer resolving to autoconfig
    - Similar work done on a shell to be able to use bearer resolving
      which is basically needed for jwt based setups.
    - Relates #3748

--- spring-cloud-dataflow-rest-client/src/main/java/org/springframework/cloud/dataflow/rest/client/config/DataFlowClientAutoConfiguration.java
+++ spring-cloud-dataflow-rest-client/src/main/java/org/springframework/cloud/dataflow/rest/client/config/DataFlowClientAutoConfiguration.java
@@ -16,6 +16,10 @@
 package org.springframework.cloud.dataflow.rest.client.config;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -23,6 +27,8 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesRegistrationAdapter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.common.security.core.support.OAuth2AccessTokenProvidingClientHttpRequestInterceptor;
 import org.springframework.cloud.dataflow.core.DataFlowPropertyKeys;
@@ -33,7 +39,19 @@ import org.springframework.cloud.dataflow.rest.client.dsl.StreamBuilder;
 import org.springframework.cloud.dataflow.rest.util.HttpClientConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.lang.Nullable;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.endpoint.DefaultClientCredentialsTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2ClientCredentialsGrantRequest;
@@ -70,6 +88,9 @@ public class DataFlowClientAutoConfiguration {
 	@Autowired
 	private @Nullable OAuth2AccessTokenResponseClient<OAuth2ClientCredentialsGrantRequest> clientCredentialsTokenResponseClient;
 
+	@Autowired
+	private @Nullable OAuth2ClientProperties oauth2ClientProperties;
+
 	@Bean
 	@ConditionalOnMissingBean(DataFlowOperations.class)
 	public DataFlowOperations dataFlowOperations() throws Exception{
@@ -91,12 +112,24 @@ public class DataFlowClientAutoConfiguration {
 		}
 		else if(!StringUtils.isEmpty(properties.getAuthentication().getBasic().getUsername()) &&
 				!StringUtils.isEmpty(properties.getAuthentication().getBasic().getPassword())){
-			httpClientConfigurer.basicAuthCredentials(properties.getAuthentication().getBasic().getUsername(), properties.getAuthentication().getBasic().getPassword());
+			httpClientConfigurer.basicAuthCredentials(properties.getAuthentication().getBasic().getUsername(),
+					properties.getAuthentication().getBasic().getPassword());
 			template.setRequestFactory(httpClientConfigurer.buildClientHttpRequestFactory());
+		}
+		else if (oauth2ClientProperties != null && !oauth2ClientProperties.getRegistration().isEmpty()
+				&& StringUtils.hasText(properties.getAuthentication().getOauth2().getUsername())
+				&& StringUtils.hasText(properties.getAuthentication().getOauth2().getPassword())) {
+			ClientHttpRequestInterceptor bearerTokenResolvingInterceptor = bearerTokenResolvingInterceptor(
+					oauth2ClientProperties, properties.getAuthentication().getOauth2().getUsername(),
+					properties.getAuthentication().getOauth2().getPassword(),
+					properties.getAuthentication().getOauth2().getClientRegistrationId());
+			template.getInterceptors().add(bearerTokenResolvingInterceptor);
+			logger.debug("Configured OAuth2 Bearer Token resolving for accessing the Data Flow Server");
 		}
 		else {
 			logger.debug("Not configuring security for accessing the Data Flow Server");
 		}
+
 		return new DataFlowTemplate(new URI(properties.getServerUri()), template);
 	}
 
@@ -128,5 +161,81 @@ public class DataFlowClientAutoConfiguration {
 		OAuth2AccessTokenResponseClient<OAuth2ClientCredentialsGrantRequest> clientCredentialsTokenResponseClient() {
 			return new DefaultClientCredentialsTokenResponseClient();
 		}
+	}
+
+	private static final Authentication DEFAULT_PRINCIPAL = createAuthentication("dataflow-client-principal");
+
+	private ClientRegistrationRepository shellClientRegistrationRepository(OAuth2ClientProperties properties) {
+		List<ClientRegistration> registrations = new ArrayList<>(
+				OAuth2ClientPropertiesRegistrationAdapter.getClientRegistrations(properties).values());
+		return new InMemoryClientRegistrationRepository(registrations);
+	}
+
+	private OAuth2AuthorizedClientService shellAuthorizedClientService(ClientRegistrationRepository shellClientRegistrationRepository) {
+		return new InMemoryOAuth2AuthorizedClientService(shellClientRegistrationRepository);
+	}
+
+	private OAuth2AuthorizedClientManager authorizedClientManager(
+			ClientRegistrationRepository shellClientRegistrationRepository,
+			OAuth2AuthorizedClientService shellAuthorizedClientService) {
+		AuthorizedClientServiceOAuth2AuthorizedClientManager manager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+			shellClientRegistrationRepository, shellAuthorizedClientService);
+		OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+			.password()
+			.refreshToken()
+			.build();
+		manager.setAuthorizedClientProvider(authorizedClientProvider);
+		manager.setContextAttributesMapper(request -> {
+			Map<String, Object> contextAttributes = new HashMap<>();
+			request.getAttributes().forEach((k, v) -> {
+				if (OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME.equals(k)
+						|| OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME.equals(k)) {
+					contextAttributes.put(k, v);
+				}
+			});
+			return contextAttributes;
+		});
+		return manager;
+	}
+
+	private ClientHttpRequestInterceptor bearerTokenResolvingInterceptor(
+			OAuth2ClientProperties properties, String username, String password, String clientRegistrationId) {
+		ClientRegistrationRepository shellClientRegistrationRepository = shellClientRegistrationRepository(properties);
+		OAuth2AuthorizedClientService shellAuthorizedClientService = shellAuthorizedClientService(shellClientRegistrationRepository);
+		OAuth2AuthorizedClientManager authorizedClientManager = authorizedClientManager(
+				shellClientRegistrationRepository, shellAuthorizedClientService);
+
+		if (properties.getRegistration() != null && properties.getRegistration().size() == 1) {
+			// if we have only one, use that
+			clientRegistrationId = properties.getRegistration().entrySet().iterator().next().getKey();
+		}
+
+		OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId)
+				.principal(DEFAULT_PRINCIPAL)
+				.attribute(OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, username)
+				.attribute(OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, password)
+				.build();
+
+		OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+		return (request, body, execution) -> {
+			request.getHeaders().setBearerAuth(authorizedClient.getAccessToken().getTokenValue());
+			return execution.execute(request, body);
+		};
+	}
+
+	private static Authentication createAuthentication(final String principalName) {
+		return new AbstractAuthenticationToken(null) {
+			private static final long serialVersionUID = -2038812908189509872L;
+
+			@Override
+			public Object getCredentials() {
+				return "";
+			}
+
+			@Override
+			public Object getPrincipal() {
+				return principalName;
+			}
+		};
 	}
 }
