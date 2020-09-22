e30a87a385282b5b1acd5b41c830aaa89320c0a9
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2020-09-22 22:57:25 +0530

    Fix tests

--- spring-cloud-dataflow-classic-docs/src/test/java/org/springframework/cloud/dataflow/server/rest/documentation/ApiDocumentation.java
+++ spring-cloud-dataflow-classic-docs/src/test/java/org/springframework/cloud/dataflow/server/rest/documentation/ApiDocumentation.java
@@ -32,6 +32,7 @@ import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.li
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -71,7 +72,7 @@ public class ApiDocumentation extends BaseDocumentation {
 
 	@Test
 	public void index() throws Exception {
-		this.mockMvc.perform(get("/")).andExpect(status().isOk()).andDo(this.documentationHandler.document(links(
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andDo(this.documentationHandler.document(links(
 				linkWithRel("about").description(
 						"Access meta information, including enabled " + "features, security info, version information"),
 
@@ -115,6 +116,7 @@ public class ApiDocumentation extends BaseDocumentation {
 				linkWithRel("streams/definitions").description("Exposes the Streams resource"),
 				linkWithRel("streams/definitions/definition").description("Handle a specific Stream definition"),
 				linkWithRel("streams/validation").description("Provides the validation for a stream definition"),
+				linkWithRel("streams/deployments").description("Provides Stream deployment operations"),
 				linkWithRel("streams/deployments/{name}").description("Request deployment info for a stream definition"),
 				linkWithRel("streams/deployments/{name}{?reuse-deployment-properties}").description("Request deployment info for a stream definition"),
 				linkWithRel("streams/deployments/deployment").description("Request (un-)deployment of an existing stream definition"),
@@ -159,12 +161,13 @@ public class ApiDocumentation extends BaseDocumentation {
 						fieldWithPath("_links.streams/logs/{streamName}.templated").type(JsonFieldType.BOOLEAN).optional().description("Link streams/logs/{streamName} is templated"),
 						fieldWithPath("_links.streams/logs/{streamName}/{appName}.templated").type(JsonFieldType.BOOLEAN).optional().description("Link streams/logs/{streamName}/{appName} is templated"),
 
+						fieldWithPath("_links.streams/deployments").description("Link to streams/deployments"),
+						fieldWithPath("_links.streams/deployments.href").description("Link to streams/deployments"),
 						fieldWithPath("_links.streams/deployments/{name}").description("Link streams/deployments/{name} is templated"),
 						fieldWithPath("_links.streams/deployments/{name}.href").description("Link streams/deployments/{name} is templated"),
 						fieldWithPath("_links.streams/deployments/{name}.templated").type(JsonFieldType.BOOLEAN).optional().description("Link streams/deployments/{name} is templated"),
 						fieldWithPath("_links.streams/deployments/{name}{?reuse-deployment-properties}.href").description("Link streams/deployments/{name} is templated"),
 						fieldWithPath("_links.streams/deployments/{name}{?reuse-deployment-properties}.templated").type(JsonFieldType.BOOLEAN).optional().description("Link streams/deployments/{name} is templated"),
-						fieldWithPath("_links.streams/deployments").type(JsonFieldType.BOOLEAN).optional().description("Link streams/deployments/{name} is templated"),
 						fieldWithPath("_links.streams/deployments/deployment.href").description("Link to the streams/deployments/deployment"),
 						fieldWithPath("_links.streams/deployments/deployment.templated").type(JsonFieldType.BOOLEAN).optional().description("Link streams/deployments/deployment is templated"),
 						fieldWithPath("_links.streams/deployments/manifest/{name}/{version}.href").description("Link to the streams/deployments/manifest/{name}/{version}"),
