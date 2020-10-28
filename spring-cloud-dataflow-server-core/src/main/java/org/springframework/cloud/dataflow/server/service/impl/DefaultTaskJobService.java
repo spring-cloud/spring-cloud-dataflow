6fa79c659d66b507409b4ba6a840334d06db9bd3
Glenn Renfro <grenfro@pivotal.io>
2020-10-28 01:49:35 -0400

    Job Props inserted by batch will now have type added on restart. (#4219)
    resolves #4216

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/service/impl/DefaultTaskJobService.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/service/impl/DefaultTaskJobService.java
@@ -240,7 +240,9 @@ public class DefaultTaskJobService implements TaskJobService {
 					}
 				}
 				if(!existsFlag) {
-					result.add(String.format("%s=%s", key,  jobParameters.getString(key)));
+					result.add(String.format("%s(%s)=%s", key,
+							jobParametersMap.get(key).getType().toString().toLowerCase(),
+							jobParameters.getString(key)));
 				}
 			}
 		}
