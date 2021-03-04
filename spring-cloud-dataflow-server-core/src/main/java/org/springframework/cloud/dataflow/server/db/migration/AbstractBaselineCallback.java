spring-cloud/spring-cloud-dataflow

commit 674c5dd9f94d16fb825a0913fe4268505826c7ea

Remove flyway transaction disable with postgres
- Previously with postgres driver there was an
  attempt not to use transactions as those always
  abort it even with query errors. This as a naive
  way would cause migration issues if something
  else goes wrong and database may be left in
  a wrong state.
- With postgres, move back to transactions and
  do a table exists check which should be more
  reliable which then seem to survive in tests
  where we manually break database and next
  launch attempt after "fixing" database looks
  to bring things in a good condition.
- These new tests are not part of this commit
  but coming in with a new integation tests
  what I've run locally.
- Fixes #4370

spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/db/migration/AbstractBaselineCallback.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,14 +57,7 @@ public abstract class AbstractBaselineCallback extends AbstractCallback {
 			commands.addAll(defaultCommands);
 		}
 
-		boolean migrateToInitial = true;
-		try {
-			JdbcTemplate jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true));
-			jdbcTemplate.execute("select 1 from APP_REGISTRATION");
-			migrateToInitial = false;
-		} catch (Exception e) {
-		}
-
+		boolean migrateToInitial = !doTableExists(context, "APP_REGISTRATION");
 		if (migrateToInitial) {
 			logger.info("Did not detect prior Data Flow schema, doing baseline.");
 			commands.addAll(initialSetupMigration.getCommands());
@@ -83,6 +76,16 @@ public abstract class AbstractBaselineCallback extends AbstractCallback {
 		}
 
 		return commands;
+	}
+
+	protected boolean doTableExists(Context context, String name) {
+		try {
+			JdbcTemplate jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true));
+			jdbcTemplate.execute("select 1 from APP_REGISTRATION");
+			return false;
+		} catch (Exception e) {
+		}
+		return true;
 	}
 
 	/**

spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/db/migration/postgresql/PostgresBeforeBaseline.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,10 +19,13 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.flywaydb.core.api.callback.Context;
-import org.flywaydb.core.api.callback.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.cloud.dataflow.common.flyway.SqlCommand;
 import org.springframework.cloud.dataflow.server.db.migration.AbstractBaselineCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 /**
  * Baselining schema setup for {@code postgres}.
@@ -32,6 +35,7 @@ import org.springframework.cloud.dataflow.server.db.migration.AbstractBaselineCa
  */
 public class PostgresBeforeBaseline extends AbstractBaselineCallback {
 
+	private static final Logger logger = LoggerFactory.getLogger(PostgresBeforeBaseline.class);
 	public final static String DROP_AUDIT_RECORDS_AUDIT_ACTION_IDX_INDEX =
 			"drop index if exists AUDIT_RECORDS_AUDIT_ACTION_IDX";
 
@@ -112,11 +116,17 @@ public class PostgresBeforeBaseline extends AbstractBaselineCallback {
 	}
 
 	@Override
-	public boolean canHandleInTransaction(Event event, Context context) {
-		// postgresql is one database where a single any error, even query error,
-		// results transaction to get aborted in a connection, so tell
-		// flyway not to use it. i.e. we check if some tables exists and this
-		// will result errors if table to check is not there.
+	protected boolean doTableExists(Context context, String name) {
+		try {
+			JdbcTemplate jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true));
+			Boolean result = jdbcTemplate.queryForObject(
+					String.format("SELECT EXISTS (SELECT relname FROM pg_class WHERE relname = '%s')", name),
+					Boolean.class);
+			logger.debug("Table {} exists {}", name, result);
+			return result != null ? result : false;
+		} catch (Exception e) {
+			logger.debug("Error checking if table exists", e);
+		}
 		return false;
 	}
 

commit de8790f6d684218aefafbdd6f4efdc6216036440

Fix mysql table migration condition for flyway
 - Fix an issue introduced by:  674c5dd9f94d16fb825a0913fe4268505826c7ea

spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/db/migration/AbstractBaselineCallback.java
@@ -26,6 +26,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.dataflow.common.flyway.AbstractCallback;
 import org.springframework.cloud.dataflow.common.flyway.SqlCommand;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 /**
@@ -81,11 +82,14 @@ public abstract class AbstractBaselineCallback extends AbstractCallback {
 	protected boolean doTableExists(Context context, String name) {
 		try {
 			JdbcTemplate jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true));
-			jdbcTemplate.execute("select 1 from APP_REGISTRATION");
-			return false;
+			jdbcTemplate.execute("select 1 from ?", (PreparedStatementCallback) preparedStatement -> {
+				preparedStatement.setString(1, name);
+				return preparedStatement.execute();
+			});
+			return true;
 		} catch (Exception e) {
 		}
-		return true;
+		return false;
 	}
 
 	/**