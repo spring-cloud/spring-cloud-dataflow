7a371d4140c10c480ace9876609ae491ce15c154
Janne Valkealahti <janne.valkealahti@gmail.com>
2021-06-07 09:43:30 +0100

    Handle task deployment properties via ctr options
    - Essentially reverts previous PR #4557 as it was a bad
      fix and broke more that it fixed.
    - This change now tries to pass in task deployment properties
      via crt composed-task-app-properties option.
    - Property key part is now base64 encoded with prefix base64_ because
      boot with its binding will silenly remove special characters from
      map keys and we could not use wildcards like deployer.*.local.working-directories-root
    - Encoded values are then decoded on ctr side.
    - One onnoying this is that these base64 encoded values are visible in a manifest but
      never directly interact with a user.
    - Fixes #4551
    
    Address review comments

---
+++ spring-cloud-dataflow-core/src/test/java/org/springframework/cloud/dataflow/core/Base64UtilsTests.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@code Base64Utils}.
+ *
+ * @author Janne Valkealahti
+ */
+public class Base64UtilsTests {
+
+	@Test
+	public void testBase64() {
+		assertThat(Base64Utils.decode(null)).isNull();
+		assertThat(Base64Utils.encode(null)).isNull();
+		assertThat(Base64Utils.decode(Base64Utils.encode("foo"))).isEqualTo("foo");
+		assertThat(Base64Utils.decode(Base64Utils.encode("foo.*.1"))).isEqualTo("foo.*.1");
+		assertThat(Base64Utils.decode("juststring")).isEqualTo("juststring");
+	}
+}
