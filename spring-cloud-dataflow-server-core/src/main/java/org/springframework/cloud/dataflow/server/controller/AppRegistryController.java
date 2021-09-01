10e84749925f638f993734706cea9b78dd80a158
Janne Valkealahti <janne.valkealahti@gmail.com>
2021-09-01 11:49:38 +0300

    Expose ssbj option groups
    - single step batch job is a monolith task app having set of readers/writers
      where combination of valid options depends on what reader and writer is
      "enabled" at any given time. order to "hint" UI we need to expose
      these set of options.
    - Using same idea how app can define inbount/outbound port names
      by properties file in META-INF, define a set of properties which
      maps into arbitrary grouping id with set of options in it.
    - File name is `dataflow-configuration-option-groups.properties` which can contain
      sections named `org.springframework.cloud.dataflow.configuration-properties.option-groups.<xxx>.<ddd>`
      where `<xxx>.<ddd>` becomes grouping id and value of it is a delimited list of options.
    - Fixes #4670

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/controller/AppRegistryController.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/controller/AppRegistryController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 the original author or authors.
+ * Copyright 2015-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -209,6 +209,9 @@ public class AppRegistryController {
 				}
 			}
 		}
+		Map<String, Set<String>> groupingsMap = this.metadataResolver
+				.listOptionGroups(this.appRegistryService.getAppMetadataResource(registration));
+		result.getOptionGroups().putAll(groupingsMap);
 		return result;
 	}
 
