a7087722e41c37705f621b49c64a39329e9ed738
Janne Valkealahti <janne.valkealahti@gmail.com>
2021-11-23 08:46:23 +0000

    Replace mysql ddls with mariadb
    - Now expecting hibernate_sequence to use proper
      sequences available from mariadb 10.3.
    - Relates #4761

--- spring-cloud-dataflow-server-core/src/main/resources/schemas/mariadb/V1-skipper.sql
+++ spring-cloud-dataflow-server-core/src/main/resources/schemas/mariadb/V1-skipper.sql
@@ -1,10 +1,4 @@
-create table if not exists hibernate_sequence (
-  next_val bigint
-);
-
-insert into hibernate_sequence (next_val)
-  select * from (select 1 as next_val) as temp
-  where not exists(select * from hibernate_sequence);
+create sequence if not exists hibernate_sequence start 1 increment 1;
 
 create table skipper_app_deployer_data (
   id bigint not null,
