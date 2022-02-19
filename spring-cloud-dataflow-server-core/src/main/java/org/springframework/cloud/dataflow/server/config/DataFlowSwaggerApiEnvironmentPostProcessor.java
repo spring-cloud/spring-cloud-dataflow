ddab0343d915e2db9a3986ba491df9a6d453c97c
Tobias Soloschenko <tsoloschenko@apache.org>
2022-02-19 14:12:44 +0100

    fix: checkstyle issues

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/config/DataFlowSwaggerApiEnvironmentPostProcessor.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/config/DataFlowSwaggerApiEnvironmentPostProcessor.java
@@ -1,15 +1,31 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.dataflow.server.config;
 
-import jdk.jpackage.internal.Log;
+import java.util.Optional;
+import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
-
-import java.util.Optional;
-import java.util.Properties;
 
 public class DataFlowSwaggerApiEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
@@ -27,7 +43,7 @@ public class DataFlowSwaggerApiEnvironmentPostProcessor implements EnvironmentPo
                 .findAny();
         // Apply default properties
         if (!apiDocsEnabledOptional.isPresent()) {
-            LOG.debug("Disable springdoc {} feature, because it is not set.",SPRINGDOC_API_DOCS_ENABLED_KEY);
+            LOG.debug("Disable springdoc {} feature, because it is not set.", SPRINGDOC_API_DOCS_ENABLED_KEY);
             applyDisableFeaturePropertySources(environment, "apiDocsDisabled", SPRINGDOC_API_DOCS_ENABLED_KEY);
         }
 
@@ -36,7 +52,7 @@ public class DataFlowSwaggerApiEnvironmentPostProcessor implements EnvironmentPo
                 .map(p -> p.getProperty(SWAGGER_UI_ENABLED_KEY))
                 .findAny();
         if (!(swaggerUiEnabledOptional.isPresent())) {
-            LOG.debug("Disable springdoc {} feature, because it is not set.",SWAGGER_UI_ENABLED_KEY);
+            LOG.debug("Disable springdoc {} feature, because it is not set.", SWAGGER_UI_ENABLED_KEY);
             applyDisableFeaturePropertySources(environment, "swaggerUiDisabled", SWAGGER_UI_ENABLED_KEY);
         }
     }
--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/support/DataFlowSwaggerApiDocsJsonDecodeFilter.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/support/DataFlowSwaggerApiDocsJsonDecodeFilter.java
@@ -1,13 +1,23 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.dataflow.server.support;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.text.StringEscapeUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.ContentCachingResponseWrapper;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -18,8 +28,16 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 @Component
 @Order
