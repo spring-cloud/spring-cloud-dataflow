7a9bdaae52b1e4ba13f9323cede29dba92a0282c
Glenn Renfro <grenfro@vmware.com>
2022-03-01 16:46:30 -0500

    CTR launched from Schedules now will suppport parent id.
    Resolves #4832

--- spring-cloud-dataflow-composed-task-runner/src/test/java/org/springframework/cloud/dataflow/composedtaskrunner/ComposedTaskRunnerConfigurationNoPropertiesTests.java
+++ spring-cloud-dataflow-composed-task-runner/src/test/java/org/springframework/cloud/dataflow/composedtaskrunner/ComposedTaskRunnerConfigurationNoPropertiesTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 the original author or authors.
+ * Copyright 2017-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 package org.springframework.cloud.dataflow.composedtaskrunner;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 
@@ -83,6 +83,6 @@ public class ComposedTaskRunnerConfigurationNoPropertiesTests {
 		assertThat(composedTaskProperties.getTransactionIsolationLevel()).isEqualTo("ISOLATION_REPEATABLE_READ");
 
 		Assert.notNull(job.getJobParametersIncrementer(), "JobParametersIncrementer must not be null.");
-		verify(taskOperations).launch("AAA", new HashMap<>(0), new ArrayList<>(0));
+		verify(taskOperations).launch("AAA", new HashMap<>(0), Collections.singletonList("--spring.cloud.task.parent-execution-id=1"));
 	}
 }
