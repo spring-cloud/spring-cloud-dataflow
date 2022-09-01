f08096b281bea1dcc5caa59a8df6aea123b648bb
Chris Bono <chris.bono@gmail.com>
2022-08-31 21:36:18 -0500

    Add support for MySQL 5/8 via MariaDB driver
    * Use FlywayVendorReplacingEnvironmentPostProcessor to replace {vendor} in spring.flyways.locations
    
    * Add back in the MySQL schema
    
    * Use vendor (mysql) not driver in flyway configuration customizer
    
    See #4887

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/db/migration/DataFlowFlywayConfigurationCustomizer.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/db/migration/DataFlowFlywayConfigurationCustomizer.java
@@ -18,37 +18,45 @@ package org.springframework.cloud.dataflow.server.db.migration;
 import javax.sql.DataSource;
 
 import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.cloud.dataflow.common.flyway.DatabaseDriverUtils;
 import org.springframework.cloud.dataflow.server.db.migration.db2.Db2BeforeBaseline;
 import org.springframework.cloud.dataflow.server.db.migration.mariadb.MariadbBeforeBaseline;
+import org.springframework.cloud.dataflow.server.db.migration.mysql.MysqlBeforeBaseline;
 import org.springframework.cloud.dataflow.server.db.migration.oracle.OracleBeforeBaseline;
 import org.springframework.cloud.dataflow.server.db.migration.postgresql.PostgresBeforeBaseline;
 import org.springframework.cloud.dataflow.server.db.migration.sqlserver.MsSqlBeforeBaseline;
-import org.springframework.jdbc.support.JdbcUtils;
-import org.springframework.jdbc.support.MetaDataAccessException;
 
 /**
  * Flyway {@link FlywayConfigurationCustomizer} bean customizing callbacks per
  * active db vendor.
  *
  * @author Janne Valkealahti
- *
+ * @author Chris Bono
  */
 public class DataFlowFlywayConfigurationCustomizer implements FlywayConfigurationCustomizer {
 
+	private static final Logger LOG = LoggerFactory.getLogger(DataFlowFlywayConfigurationCustomizer.class);
+
 	@Override
 	public void customize(FluentConfiguration configuration) {
-		// boot's flyway auto-config doesn't allow to define callbacks per
+		// Boot's flyway auto-config doesn't allow to define callbacks per
 		// vendor id, so essentially customizing those here.
 		DataSource dataSource = configuration.getDataSource();
-		DatabaseDriver databaseDriver = getDatabaseDriver(dataSource);
+		DatabaseDriver databaseDriver = DatabaseDriverUtils.getDatabaseDriver(dataSource);
+		LOG.info("Adding vendor specific Flyway callback for {}", databaseDriver.name());
 		if (databaseDriver == DatabaseDriver.POSTGRESQL) {
 			configuration.callbacks(new PostgresBeforeBaseline());
 		}
 		else if (databaseDriver == DatabaseDriver.MARIADB) {
 			configuration.callbacks(new MariadbBeforeBaseline());
+		}
+		else if (databaseDriver == DatabaseDriver.MYSQL) {
+			configuration.callbacks(new MysqlBeforeBaseline());
 		}
 		else if (databaseDriver == DatabaseDriver.SQLSERVER) {
 			configuration.callbacks(new MsSqlBeforeBaseline());
@@ -58,17 +66,6 @@ public class DataFlowFlywayConfigurationCustomizer implements FlywayConfiguratio
 		}
 		else if (databaseDriver == DatabaseDriver.DB2) {
 			configuration.callbacks(new Db2BeforeBaseline());
-		}
-	}
-
-	private DatabaseDriver getDatabaseDriver(DataSource dataSource) {
-		// copied from boot's flyway auto-config to get matching db vendor id
-		try {
-			String url = JdbcUtils.extractDatabaseMetaData(dataSource, "getURL");
-			return DatabaseDriver.fromJdbcUrl(url);
-		}
-		catch (MetaDataAccessException ex) {
-			throw new IllegalStateException(ex);
 		}
 	}
 }
