9a32027f451c4275c961ff790a9844778acaeef6
Chris Bono <chris.bono@gmail.com>
2022-09-02 17:46:53 -0500

    Add support for MySQL 5/8 via MariaDB driver (#1059)
    * Use FlywayVendorReplacingEnvironmentPostProcessor to replace {vendor} in 'spring.flyway.locations'
    * Add back in the MySQL schema
    * Use actual db (not driver) in flyway configuration customizer
    
    See https://github.com/spring-cloud/spring-cloud-dataflow/issues/4887

--- spring-cloud-skipper-server-core/src/main/java/org/springframework/cloud/skipper/server/db/migration/SkipperFlywayConfigurationCustomizer.java
+++ spring-cloud-skipper-server-core/src/main/java/org/springframework/cloud/skipper/server/db/migration/SkipperFlywayConfigurationCustomizer.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.cloud.skipper.server.db.migration;
 
 import javax.sql.DataSource;
@@ -23,37 +24,40 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.cloud.dataflow.common.flyway.DatabaseDriverUtils;
 import org.springframework.cloud.skipper.server.db.migration.db2.Db2BeforeBaseline;
 import org.springframework.cloud.skipper.server.db.migration.mariadb.MariadbBeforeBaseline;
+import org.springframework.cloud.skipper.server.db.migration.mysql.MysqlBeforeBaseline;
 import org.springframework.cloud.skipper.server.db.migration.oracle.OracleBeforeBaseline;
 import org.springframework.cloud.skipper.server.db.migration.postgresql.PostgresBeforeBaseline;
 import org.springframework.cloud.skipper.server.db.migration.sqlserver.MsSqlBeforeBaseline;
-import org.springframework.jdbc.support.JdbcUtils;
-import org.springframework.jdbc.support.MetaDataAccessException;
 
 /**
  * Flyway {@link FlywayConfigurationCustomizer} bean customizing callbacks per
  * active db vendor.
  *
  * @author Janne Valkealahti
- *
+ * @author Chris Bono
  */
 public class SkipperFlywayConfigurationCustomizer implements FlywayConfigurationCustomizer {
 
-	private static final Logger logger = LoggerFactory.getLogger(SkipperFlywayConfigurationCustomizer.class);
+	private static final Logger LOG = LoggerFactory.getLogger(SkipperFlywayConfigurationCustomizer.class);
 
 	@Override
 	public void customize(FluentConfiguration configuration) {
-		// boot's flyway auto-config doesn't allow to define callbacks per
+		// Boot's flyway auto-config doesn't allow to define callbacks per
 		// vendor id, so essentially customizing those here.
 		DataSource dataSource = configuration.getDataSource();
-		DatabaseDriver databaseDriver = getDatabaseDriver(dataSource);
-		logger.info("Customizing flyway config, detected DatabaseDriver as {}.", databaseDriver);
+		DatabaseDriver databaseDriver = DatabaseDriverUtils.getDatabaseDriver(dataSource);
+		LOG.info("Adding vendor specific Flyway callback for {}", databaseDriver.name());
 		if (databaseDriver == DatabaseDriver.POSTGRESQL) {
 			configuration.callbacks(new PostgresBeforeBaseline());
 		}
 		else if (databaseDriver == DatabaseDriver.MARIADB) {
 			configuration.callbacks(new MariadbBeforeBaseline());
+		}
+		else if (databaseDriver == DatabaseDriver.MYSQL) {
+			configuration.callbacks(new MysqlBeforeBaseline());
 		}
 		else if (databaseDriver == DatabaseDriver.SQLSERVER) {
 			configuration.callbacks(new MsSqlBeforeBaseline());
@@ -63,17 +67,6 @@ public class SkipperFlywayConfigurationCustomizer implements FlywayConfiguration
 		}
 		else if (databaseDriver == DatabaseDriver.DB2) {
 			configuration.callbacks(new Db2BeforeBaseline());
-		}
-	}
-
-	private DatabaseDriver getDatabaseDriver(DataSource dataSource) {
-		// copied from boot's flyway auto-config to get matching db vendor id
-		try {
-			String url = JdbcUtils.extractDatabaseMetaData(dataSource, "getURL");
-			return DatabaseDriver.fromJdbcUrl(url);
-		}
-		catch (MetaDataAccessException ex) {
-			throw new IllegalStateException(ex);
 		}
 	}
 }
