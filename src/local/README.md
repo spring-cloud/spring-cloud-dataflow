ac05c3e05c1e11da4686ff37f8b831e720a12fa8
Corneil du Plessis <pcorneil@vmware.com>
2022-09-27 13:51:09 +0200

    Update to use for and timestamp on snapshots. (#5103)

--- src/local/README.md
+++ src/local/README.md
@@ -2,8 +2,19 @@
 
 The files in this folder are to support local testing and development.
 
+These scripts are useful when you want to build the containers on a different architecture and publish to a private repo.
+
+## `download-apps.sh`
+Downloads all applications needed by `create-containers.sh` from Maven repository.
+
+*If the timestamp of snapshots matches the download will be skipped.*
+
+Usage: `download-apps.sh [version]`
+
 ## `create-containers.sh`
 Creates all containers and pushes to local docker registry.
+
+Usage: `create-containers.sh [version] [jre-version]`
 
 ## `simple-integration-test.sh`
 
--- src/local/create-containers.sh
+++ src/local/create-containers.sh
@@ -1,25 +1,48 @@
 #!/usr/bin/env bash
-./mvnw clean install -Pspring
-TAG=2.10.0-SNAPSHOT
-v=11
-pack build \
-  --path spring-cloud-dataflow-server/target/spring-cloud-dataflow-server-$TAG.jar \
-  --builder gcr.io/paketo-buildpacks/builder:base \
-  --env BP_JVM_VERSION=$v springcloud/spring-cloud-dataflow-server:$TAG
-pack build \
-  --path spring-cloud-dataflow-composed-task-runner/target/spring-cloud-dataflow-composed-task-runner-$TAG.jar \
-  --builder gcr.io/paketo-buildpacks/builder:base \
-  --env BP_JVM_VERSION=$v springcloud/spring-cloud-dataflow-composed-task-runner:$TAG
-pack build \
-  --path spring-cloud-dataflow-tasklauncher/spring-cloud-dataflow-tasklauncher-sink-kafka/target/spring-cloud-dataflow-tasklauncher-sink-kafka-$TAG.jar \
-  --builder gcr.io/paketo-buildpacks/builder:base \
-  --env BP_JVM_VERSION=$v springcloud/spring-cloud-dataflow-tasklauncher-sink-kafka:$TAG
-pack build \
-  --path spring-cloud-dataflow-tasklauncher/spring-cloud-dataflow-tasklauncher-sink-rabbit/target/spring-cloud-dataflow-tasklauncher-sink-rabbit-$TAG.jar \
-  --builder gcr.io/paketo-buildpacks/builder:base \
-  --env BP_JVM_VERSION=$v springcloud/spring-cloud-dataflow-tasklauncher-sink-rabbit:$TAG
-pack build \
-  --path spring-cloud-dataflow-single-step-batch-job/target/spring-cloud-dataflow-single-step-batch-job-$TAG.jar \
-  --builder gcr.io/paketo-buildpacks/builder:base \
-  --env BP_JVM_VERSION=$v springcloud/spring-cloud-dataflow-single-step-batch-job:$TAG
+SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+SCDIR=$(realpath $SCDIR)
+ROOT_DIR=$(realpath $SCDIR/../..)
+
+# set to specific version
+if [ "$1" != "" ]; then
+    TAG=$1
+else
+    TAG=2.10.0-SNAPSHOT
+fi
+if [ "$2" != "" ]; then
+    v=$2
+else
+    v=11
+fi
+
+# set with extra option for buildpacks. BP_OPTIONS=
+
+APPS=("spring-cloud-dataflow-server" "spring-cloud-dataflow-composed-task-runner" "spring-cloud-dataflow-single-step-batch-job")
+for app in ${APPS[@]}; do
+    APP_PATH="$ROOT_DIR/$app/target"
+    if [ ! -f "$APP_PATH/$app-$TAG.jar" ]; then
+        echo "Cannot find $APP_PATH/$app-$TAG.jar download using download-apps.sh or build using ./mvnw install"
+        exit 1
+    fi
+    pack build \
+        --path $APP_PATH/$app-$TAG.jar \
+        --builder gcr.io/paketo-buildpacks/builder:base \
+        --env BP_JVM_VERSION=$v \
+        $BP_OPTIONS springcloud/$app:$TAG
+done
+TS_APPS=("spring-cloud-dataflow-tasklauncher-sink-kafka" "spring-cloud-dataflow-tasklauncher-sink-rabbit")
+for app in ${TS_APPS[@]}; do
+    APP_PATH="$ROOT_DIR/spring-cloud-dataflow-tasklauncher/$app/target"
+    if [ ! -f "$APP_PATH/$app-$TAG.jar" ]; then
+        echo "Cannot find $APP_PATH/$app-$TAG.jar download using download-apps.sh or build using ./mvnw install"
+        exit 1
+    fi
+    pack build \
+        --path $APP_PATH/$app-$TAG.jar \
+        --builder gcr.io/paketo-buildpacks/builder:base \
+        --env BP_JVM_VERSION=$v \
+        $BP_OPTIONS springcloud/$app:$TAG
+done
+pushd $ROOT_DIR > /dev/null
 docker build -t springcloud/spring-cloud-dataflow-prometheus-local:$TAG src/grafana/prometheus/docker/prometheus-local
+popd > /dev/null
