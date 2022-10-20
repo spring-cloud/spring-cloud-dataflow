cebd239fa20d91f20d422f54fd57eac74d8e9bf1
tassoc <claudio.tasso@tasgroup.eu>
2022-10-20 14:49:50 +0200

    Added index 'TASK_EXECUTION_ID_IDX'
    https://github.com/spring-cloud/spring-cloud-dataflow/issues/5122

---
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/db/migration/postgresql/V5__Add_Task_Execution_Params_Indexes.java
@@ -0,0 +1,30 @@
+package org.springframework.cloud.dataflow.server.db.migration.postgresql;
+
+import java.util.Arrays;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.cloud.dataflow.common.flyway.SqlCommand;
+import org.springframework.cloud.dataflow.common.flyway.SqlCommandsRunner;
+
+/**
+ * This migration class adds index for TASK_EXECUTION_ID on TASK_EXECUTION_PARAMS.
+ *
+ * @author Claudio Tasso
+ *
+ * @since 2.10
+ */
+public class V5__Add_Task_Execution_Params_Indexes extends BaseJavaMigration {
+
+	public final static String ADD_INDEX_TO_STEP_EXECUTION_PARAMS = "create index TASK_EXECUTION_ID_IDX on TASK_EXECUTION_PARAMS (TASK_EXECUTION_ID)";
+
+	private final SqlCommandsRunner runner = new SqlCommandsRunner();
+
+	@Override
+	public void migrate(Context context) throws Exception {
+		runner.execute(context.getConnection(), Arrays.asList(
+				SqlCommand.from(ADD_INDEX_TO_STEP_EXECUTION_PARAMS)));
+	
+
+	}
+}
