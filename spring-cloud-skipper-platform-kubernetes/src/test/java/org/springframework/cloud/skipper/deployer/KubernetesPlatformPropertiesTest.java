649002ff9e4d0b5f6fdb8fee3c9dbb82d8451e57
Chris Bono <chris.bono@gmail.com>
2022-11-07 21:12:30 -0600

    Fix failure in KubernetesPlatformPropertiesTest
    - Add 'spring.cloud.kubernetes.client.namespace' prop which
    is required in SCK 2021.0.5

--- spring-cloud-skipper-platform-kubernetes/src/test/java/org/springframework/cloud/skipper/deployer/KubernetesPlatformPropertiesTest.java
+++ spring-cloud-skipper-platform-kubernetes/src/test/java/org/springframework/cloud/skipper/deployer/KubernetesPlatformPropertiesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2017-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,9 +37,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Donovan Muller
+ * @author Chris Bono
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = KubernetesPlatformPropertiesTest.TestConfig.class)
+@SpringBootTest(classes = KubernetesPlatformPropertiesTest.TestConfig.class,
+		properties = { "spring.cloud.kubernetes.client.namespace=default" })
 @ActiveProfiles("platform-properties")
 public class KubernetesPlatformPropertiesTest {
 
