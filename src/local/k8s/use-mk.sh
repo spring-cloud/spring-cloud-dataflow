99aff9d4ca9a0b2632aa1ed391f9d642bc61352a
Corneil du Plessis <pcorneil@vmware.com>
2023-01-19 16:36:45 +0200

    Remove repo.spring.io/release Fix references to libs-snapshot.

--- src/local/k8s/use-mk.sh
+++ src/local/k8s/use-mk.sh
@@ -5,15 +5,46 @@ if [ "$sourced" = "0" ]; then
   exit 1
 fi
 if [ "$1" = "" ]; then
-  echo "Expected parameter providing MiniKube driver type."
-  echo "Choose one of kvm2, docker, vmware, virtualbox, podman, vmwarefusion, hyperkit"
+  echo "Arguments <driver> [--namespace <namespace>] [database] [broker]"
+  echo "Driver must be one of kind, Or a valid driver for minikube like kvm2, docker, vmware, virtualbox, podman, vmwarefusion, hyperkit"
   return 2
 fi
 export K8S_DRIVER=$1
 export KUBECONFIG=
-if [ "$2" = "" ]; then
-  export NS=default
-else
-  export NS=$2
-fi
+export NS=default
+shift
+while [ "$1" != "" ]; do
+    case "$1" in
+    "postgres" | "postgresql")
+      export DATABASE=postgresql
+      ;;
+  "maria" | "mariadb")
+        export DATABASE=mariadb
+        ;;
+    "rabbit" | "rabbitmq")
+        export BROKER=rabbitmq
+        ;;
+    "kafka")
+        export  BROKER=kafka
+        ;;
+    "--namespace" | "-ns")
+        if [ "$2" == "" ]; then
+            echo "Expected <namespace> after $1"
+            exit 0
+        fi
+        export NS=$2
+        shift
+        ;;
+    *)
+        echo "Unknown option $2"
+        exit 0
+    esac
+    shift
+done
 echo "Namespace: $NS"
+if [ "$BROKER" != "" ]; then
+    echo "Broker: $BROKER"
+fi
+if [ "$DATABASE" != "" ]; then
+    echo "Database: $DATABASE"
+fi
