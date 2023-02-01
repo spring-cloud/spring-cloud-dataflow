a1834e9686d898c7682738b11e4a9555787f868a
Glenn Renfro <grenfro@vmware.com>
2023-02-01 10:15:13 -0500

    Updated version for prometheus proxy and updated delete script
    Script now removes cluster roles, cluster role bindings, and service account for prometheus if user sets PROMETHEUS to true.
    Updated proxy to 1.5 from 0.11.0

--- src/local/k8s/delete-scdf.sh
+++ src/local/k8s/delete-scdf.sh
@@ -10,7 +10,15 @@ kubectl delete configmap --all $WAIT --namespace="$NS"
 kubectl delete pvc --all $WAIT --namespace="$NS"
 kubectl delete secrets --namespace "$NS" --all
 kubectl delete pvc --namespace "$NS" --all
-
+if [ "$PROMETHEUS" = "true" ]; then
+    echo "Removing ClusterRoles, ClusterRoleBindings, and ServiceAccounts for prometheus"
+    kubectl delete clusterroles prometheus
+    kubectl delete clusterrolebindings prometheus
+    kubectl delete serviceaccounts prometheus --namespace "$NS"
+    kubectl delete clusterroles prometheus-proxy
+    kubectl delete clusterrolebindings prometheus-proxy
+    kubectl delete serviceaccounts prometheus-proxy --namespace "$NS"
+fi
 if [ "$K8S_DRIVER" != "tmc" ] && [ "$K8S_DRIVER" != "gke" ] ; then
   echo "stopping port forward"
   kubectl_pid=$(ps aux | grep 'kubectl' | grep 'port\-forward' | awk '{print $2}')
