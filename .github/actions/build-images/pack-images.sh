de30a04ca13f3f12bf4b6f7e7c68e02e105e8269
Corneil du Plessis <pcorneil@vmware.com>
2023-03-24 16:06:15 +0200

    Update build-images.

---
+++ .github/actions/build-images/pack-images.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+if [ "$TAG" == "" ]; then
+    echo "TAG not found"
+    exit 1
+fi
+
+function pack_image {
+    PATH="$1"
+    REPO="$2"
+    echo "Packaging $PATH"
+    for v in 8 11 17; do
+
+        echo "Creating: $REPO:$TAG-jdk$v"
+        /usr/bin/pack build --builder gcr.io/paketo-buildpacks/builder:base \
+            --path "$PATH-$TAG.jar" \
+            --env BP_JVM_VERSION=$v "$REPO:$TAG-jdk$v"
+        echo "Created: $REPO:$TAG-jdk$v"
+
+    done
+}
+
+pack_image "spring-cloud-dataflow-server/target/spring-cloud-dataflow-server" "springcloud/spring-cloud-dataflow-server"
+pack_image "spring-cloud-dataflow-composed-task-runner/target/spring-cloud-dataflow-composed-task-runner" "springcloud/spring-cloud-dataflow-composed-task-runner"
+pack_image "spring-cloud-dataflow-tasklauncher/spring-cloud-dataflow-tasklauncher-sink-kafka/target/spring-cloud-dataflow-tasklauncher-sink-kafka" "springcloud/spring-cloud-dataflow-tasklauncher-sink-kafka"
+pack_image "spring-cloud-dataflow-tasklauncher/spring-cloud-dataflow-tasklauncher-sink-rabbit/target/spring-cloud-dataflow-tasklauncher-sink-rabbit" "springcloud/spring-cloud-dataflow-tasklauncher-sink-rabbit"
+pack_image "spring-cloud-dataflow-single-step-batch-job/target/spring-cloud-dataflow-single-step-batch-job" "springcloud/spring-cloud-dataflow-single-step-batch-job"
