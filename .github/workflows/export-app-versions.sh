c954fbc7f866065f3edcc936289b6a0e1d76a5f6
Corneil du Plessis <pcorneil@vmware.com>
2023-05-22 14:14:36 +0200

    [CI] Fix export-app-versions.sh

--- .github/workflows/export-app-versions.sh
+++ .github/workflows/export-app-versions.sh
@@ -6,6 +6,7 @@ if ((RC!=0)); then
     echo "DATAFLOW_VERSION=$DATAFLOW_VERSION"
     exit $RC
 fi
+echo "DATAFLOW_VERSION=$DATAFLOW_VERSION"
 SKIPPER_VERSION=$(mvn help:evaluate -Dexpression=spring-cloud-skipper.version  -pl spring-cloud-dataflow-parent -q -DforceStdout)
 if [[ "$SKIPPER_VERSION" = *"ERROR"* ]]; then
     SKIPPER_VERSION=$(mvn help:evaluate -Dexpression=spring-cloud-skipper.version -q -DforceStdout)
@@ -15,12 +16,14 @@ if ((RC!=0)); then
     echo "SKIPPER_VERSION=$SKIPPER_VERSION"
     exit $RC
 fi
+echo "SKIPPER_VERSION=$SKIPPER_VERSION"
 DATAFLOW_VERSION_NOPOSTFIX=$(echo '${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}' | mvn build-helper:parse-version help:evaluate -q -DforceStdout)
 RC=$?
 if ((RC!=0)); then
     echo "DATAFLOW_VERSION_NOPOSTFIX=$DATAFLOW_VERSION_NOPOSTFIX"
     exit $RC
 fi
+echo "DATAFLOW_VERSION_NOPOSTFIX=$DATAFLOW_VERSION_NOPOSTFIX"
 export DATAFLOW_VERSION
 export SKIPPER_VERSION
 export DATAFLOW_VERSION_NOPOSTFIX
