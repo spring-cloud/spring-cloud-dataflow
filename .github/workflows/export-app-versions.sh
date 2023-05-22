eba923bf2a7703c15252fc38fca77dc23282f0f0
Corneil du Plessis <pcorneil@vmware.com>
2023-05-22 14:03:34 +0200

    [CI] Fix export-app-versions.sh

--- .github/workflows/export-app-versions.sh
+++ .github/workflows/export-app-versions.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-export DATAFLOW_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-export SKIPPER_VERSION=$(mvn help:evaluate -Dexpression=spring-cloud-skipper.version  -pl spring-cloud-dataflow-parent -q -DforceStdout)
-export DATAFLOW_VERSION_NOPOSTFIX=$(echo '${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}' | mvn build-helper:parse-version help:evaluate -q -DforceStdout)
+DATAFLOW_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+SKIPPER_VERSION=$(mvn help:evaluate -Dexpression=spring-cloud-skipper.version  -pl spring-cloud-dataflow-parent -q -DforceStdout)
+if [[ "$SKIPPER_VERSION" = *"ERROR"* ]]; then
+    SKIPPER_VERSION=$(mvn help:evaluate -Dexpression=spring-cloud-skipper.version -q -DforceStdout)
+fi
+
+DATAFLOW_VERSION_NOPOSTFIX=$(echo '${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}' | mvn build-helper:parse-version help:evaluate -q -DforceStdout)
+
+export DATAFLOW_VERSION
+export SKIPPER_VERSION
+export DATAFLOW_VERSION_NOPOSTFIX
 
 echo "DATAFLOW_VERSION=$DATAFLOW_VERSION"
 echo "SKIPPER_VERSION=$SKIPPER_VERSION"
