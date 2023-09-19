48f53a6b6f7376ebc58f25182f6e69757f7d9568
Corneil du Plessis <pcorneil@vmware.com>
2023-09-19 15:31:51 +0200

    Fix generation of javadoc.

--- build-docs.sh
+++ build-docs.sh
@@ -2,7 +2,7 @@
 SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 SCDIR=$(realpath $SCDIR)
 pushd "$SCDIR" > /dev/null || exit
-    ./mvnw install -o -am -pl :spring-cloud-dataflow-classic-docs,:spring-cloud-dataflow-docs,:spring-cloud-skipper-server-core,:spring-cloud-skipper-docs -DskipTests
+    ./mvnw install -DskipTests -T 1C
     ./mvnw install -o -Pfull,asciidoctordocs,restdocs -pl :spring-cloud-dataflow-classic-docs,:spring-cloud-dataflow-docs
     ./mvnw install -o -Pasciidoctordocs,restdocs -pl :spring-cloud-skipper-server-core,:spring-cloud-skipper-docs
 popd > /dev/null || exit
