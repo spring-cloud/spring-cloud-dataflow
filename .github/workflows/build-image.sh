f88b16ab1d67d3b1d6e7e7510cff5e2d3651c13c
Corneil du Plessis <pcorneil@vmware.com>
2023-09-19 23:08:18 +0200

    Create build-images job.

--- .github/workflows/build-image.sh
+++ .github/workflows/build-image.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 set +e
 if [ "$TAG" == "" ]; then
     echo "TAG not found"
@@ -10,64 +11,23 @@ if [ "$DEFAULT_JDK" = "" ]; then
 else
     echo "DEFAULT_JDK=$DEFAULT_JDK"
 fi
-xmllint --version
-RC=$?
-if((RC!=0)); then
-    echo "xmllint not found"
-    exit $RC
-fi
+
 function download_image() {
     TARGET=$1
     ARTIFACT_ID=$2
     VERSION=$3
     TARGET_FILE=$TARGET/$ARTIFACT_ID-$VERSION.jar
-    if [[ "$VERSION" = *"-SNAPSHOT"* ]]; then
-        META_DATA="https://repo.spring.io/snapshot/org/springframework/cloud/$ARTIFACT_ID/${VERSION}/maven-metadata.xml"
-        echo "Downloading $META_DATA"
-        rm -f maven-metadata.xml
-        COUNT=5
-        RC=0
-        while ((COUNT>0)); do
-            wget -O maven-metadata.xml -q "$META_DATA"
-            RC=$?
-            if((RC==0)); then
-                break;
-            fi
-        done
-        if((RC!=0)); then
+    pushd $SCDIR/download-jar > /dev/null || exit
+        ./gradlew downloadJar -PartifactId=$ARTIFACT_ID -PartifactVersion=$VERSION -PartifactPath=$TARGET
+        RC=$?
+        if((RC != 0)); then
             exit $RC
         fi
-        xmllint --xpath "/metadata/versioning/snapshot/timestamp/text()" maven-metadata.xml | sed 's/\.//'
-        DL_TS=$(xmllint --xpath "/metadata/versioning/snapshot/timestamp/text()" maven-metadata.xml | sed 's/\.//')
-        echo "Metadata: $DL_TS"
-        DL_VERSION=$(xmllint --xpath "/metadata/versioning/snapshotVersions/snapshotVersion[extension/text() = 'pom' and updated/text() = '$DL_TS']/value/text()" maven-metadata.xml)
-        URL="https://repo.spring.io/snapshot/org/springframework/cloud/$ARTIFACT_ID/${VERSION}/$ARTIFACT_ID-${DL_VERSION}.jar"
-    else
-        REL_TYPE=
-        if [[ "$STREAM_APPS_VERSION" = *"-M"* ]] || [[ "$STREAM_APPS_VERSION" = *"-RC"* ]]; then
-            REL_TYPE=milestone
-        fi
-        if [ "$REL_TYPE" != "" ]; then
-            URL="https://repo.spring.io/$REL_TYPE/org/springframework/cloud/$ARTIFACT_ID/${VERSION}/$ARTIFACT_ID-${VERSION}.jar"
-        else
-            URL="https://repo.maven.apache.org/maven2/org/springframework/cloud/$ARTIFACT_ID/${VERSION}/$ARTIFACT_ID-${VERSION}.jar"
-        fi
-    fi
-    END=$(date '+%s')
-    END=$((600 + END))
-    RC=0
-    mkdir -p $TARGET
-    while ((END > $(date '+%s') )); do
-        echo "Downloading $URL"
-        rm -f "$TARGET_FILE"
-        wget -O "$TARGET_FILE" -q "$URL"
-        RC=$?
-        if((RC == 0)); then
-            break;
-        fi
-    done
-    if((RC != 0)); then
-        exit $RC
+    popd > /dev/null || exit
+    if [ ! -f $TARGET_FILE ]; then
+        echo "Cannot find $TARGET_FILE"
+        ls -al $TARGET
+        exit 2
     fi
     echo "Downloaded $TARGET_FILE"
 }
