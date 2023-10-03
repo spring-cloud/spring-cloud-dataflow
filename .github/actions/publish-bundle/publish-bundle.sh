e32f22f1db3b49f443fb5df63ff73b572cab9b29
Corneil du Plessis <pcorneil@vmware.com>
2023-10-03 22:03:58 +0200

    [CI] Update for SRP

--- .github/actions/publish-bundle/publish-bundle.sh
+++ .github/actions/publish-bundle/publish-bundle.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
-imgpkg push --bundle $REPOSITORY:$VERSION-RANDOM.$RTAG --file $BUNDLE_PATH
+IMG_PKG_OPT=
+if [ "$USE_SRP" == "true" ]; then
+    IMG_PKG_OPT=--debug
+fi
+imgpkg push $IMG_PKG_OPT --bundle $REPOSITORY:$VERSION-RANDOM.$RTAG --file $BUNDLE_PATH
 docker pull $REPOSITORY:$VERSION-RANDOM.$RTAG
 docker tag $REPOSITORY:$VERSION-RANDOM.$RTAG $REPOSITORY:$VERSION
 docker push $REPOSITORY:$VERSION
