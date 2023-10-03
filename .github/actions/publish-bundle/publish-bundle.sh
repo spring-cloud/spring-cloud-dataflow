196a73b233a120d4f8ef6c8d754263a934bb0abd
Glenn Renfro <grenfro@vmware.com>
2023-10-03 19:34:22 -0400

    Add diagnostic information for publish-bundle.sh
    This is for diangosing the observer failure for imgpkg

--- .github/actions/publish-bundle/publish-bundle.sh
+++ .github/actions/publish-bundle/publish-bundle.sh
@@ -3,6 +3,9 @@ set -e
 IMG_PKG_OPT=
 if [ "$USE_SRP" == "true" ]; then
     IMG_PKG_OPT="--debug --registry-verify-certs"
+# TODO This is for diagnostic purposes.  It will be removed
+    ls -al /tmp/srp-tools/observer/bin/runtime/logs/
+
 fi
 # --registry-ca-cert-path strings
 #
@@ -10,3 +13,12 @@ imgpkg push $IMG_PKG_OPT --bundle $REPOSITORY:$VERSION-RANDOM.$RTAG --file $BUND
 docker pull $REPOSITORY:$VERSION-RANDOM.$RTAG
 docker tag $REPOSITORY:$VERSION-RANDOM.$RTAG $REPOSITORY:$VERSION
 docker push $REPOSITORY:$VERSION
+
+# TODO This is for diagnostic purposes.  It will be removed.
+if [ "$USE_SRP" == "true" ]; then
+    echo "=================>>>>>>>>observer logs<<<<<<<<<<=============="
+    ls -al /tmp/srp-tools/observer/bin/runtime/logs/
+    cat /tmp/srp-tools/observer/bin/runtime/logs/access.log
+    cat /tmp/srp-tools/observer/bin/runtime/logs/mitmdump_error.log
+    echo "=================>>>>>>>>end observer logs<<<<<<<<<<=========="
+fi
