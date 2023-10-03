fce88c30c1432e851671790ec3d49f958b5ddd7d
Glenn Renfro <grenfro@vmware.com>
2023-10-03 19:48:39 -0400

    Remove diagnostic code from publish-bundle.

--- .github/actions/publish-bundle/publish-bundle.sh
+++ .github/actions/publish-bundle/publish-bundle.sh
@@ -3,9 +3,6 @@ set -e
 IMG_PKG_OPT=
 if [ "$USE_SRP" == "true" ]; then
     IMG_PKG_OPT="--debug --registry-verify-certs"
-# TODO This is for diagnostic purposes.  It will be removed
-    ls -al /tmp/srp-tools/observer/bin/runtime/logs/
-
 fi
 # --registry-ca-cert-path strings
 #
@@ -13,12 +10,3 @@ imgpkg push $IMG_PKG_OPT --bundle $REPOSITORY:$VERSION-RANDOM.$RTAG --file $BUND
 docker pull $REPOSITORY:$VERSION-RANDOM.$RTAG
 docker tag $REPOSITORY:$VERSION-RANDOM.$RTAG $REPOSITORY:$VERSION
 docker push $REPOSITORY:$VERSION
-
-# TODO This is for diagnostic purposes.  It will be removed.
-if [ "$USE_SRP" == "true" ]; then
-    echo "=================>>>>>>>>observer logs<<<<<<<<<<=============="
-    ls -al /tmp/srp-tools/observer/bin/runtime/logs/
-    cat /tmp/srp-tools/observer/bin/runtime/logs/access.log
-    cat /tmp/srp-tools/observer/bin/runtime/logs/mitmdump_error.log
-    echo "=================>>>>>>>>end observer logs<<<<<<<<<<=========="
-fi
