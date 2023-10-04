970cf9481f009c046d4bb5517dd2f8febfa47569
Corneil du Plessis <pcorneil@vmware.com>
2023-10-04 12:31:12 +0200

    [CI] Update registry-ca-cert-path

--- .github/actions/publish-bundle/publish-bundle.sh
+++ .github/actions/publish-bundle/publish-bundle.sh
@@ -2,10 +2,18 @@
 set -e
 IMG_PKG_OPT=
 if [ "$USE_SRP" == "true" ]; then
-    IMG_PKG_OPT="--debug --registry-verify-certs=false"
+    IMG_PKG_OPT="--debug"
+    if [ "$NODE_EXTRA_CA_CERTS" != "" ]; then
+        IMG_PKG_OPT="$IMG_PKG_OPT --registry-ca-cert-path $NODE_EXTRA_CA_CERTS"
+    else
+        IMG_PKG_OPT="$IMG_PKG_OPT --registry-verify-certs=false"
+    fi
 fi
+if [ "$IMG_PKG_OPT" != "" ]; then
+    echo "IMG_PKG_OPT=$IMG_PKG_OPT"
+fi
+imgpkg push $IMG_PKG_OPT --bundle "$REPOSITORY:$VERSION-RANDOM.$RTAG" --file "$BUNDLE_PATH"
+docker pull "$REPOSITORY:$VERSION-RANDOM.$RTAG"
+docker tag "$REPOSITORY:$VERSION-RANDOM.$RTAG $REPOSITORY:$VERSION"
+docker push "$REPOSITORY:$VERSION"
 
-imgpkg push $IMG_PKG_OPT --bundle $REPOSITORY:$VERSION-RANDOM.$RTAG --file $BUNDLE_PATH
-docker pull $REPOSITORY:$VERSION-RANDOM.$RTAG
-docker tag $REPOSITORY:$VERSION-RANDOM.$RTAG $REPOSITORY:$VERSION
-docker push $REPOSITORY:$VERSION
