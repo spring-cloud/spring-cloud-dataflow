4eceed39353a238fc165ec5d2fe871e000deeaff
Chris Bono <cbono@vmware.com>
2023-11-07 13:37:29 -0600

    Add support for running Oracle locally on Apple ARM64
    * Adds `local-dev-oracle` Maven profile that can be used to build a server jar
    that includes the Oracle database driver for local testing only.
    
    * Introduces SystemStubsExtension for setting testcontainers required env vars
    when running tests on Mac M
    
    * Decorates the OracleSmokeTest with the above which serves as an example of how
     to conditionally run an Oracle testcontainers test on Mac M.
    
    * In tandem, a new page was added to thw Wiki https://github.com/spring-cloud/spring-cloud-dataflow/wiki/Oracle-on-Mac-ARM64
    
    Resolves #5539

--- spring-cloud-dataflow-server/src/test/java/org/springframework/cloud/dataflow/server/db/migration/OracleSmokeTest.java
+++ spring-cloud-dataflow-server/src/test/java/org/springframework/cloud/dataflow/server/db/migration/OracleSmokeTest.java
@@ -15,22 +15,50 @@
  */
 package org.springframework.cloud.dataflow.server.db.migration;
 
+import java.util.Locale;
+
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.utility.DockerImageName;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-
+import org.springframework.core.log.LogAccessor;
 
 /**
  * Basic database schema and JPA tests for Oracle XE.
  *
  * @author Corneil du Plessis
+ * @author Chris Bono
  */
+@ExtendWith(SystemStubsExtension.class)
 public class OracleSmokeTest extends AbstractSmokeTest {
+
+	private static final LogAccessor LOG = new LogAccessor(OracleSmokeTest.class);
+
+	private static final String WIKI_LINK = "https://github.com/spring-cloud/spring-cloud-dataflow/wiki/Oracle-on-Mac-ARM64";
+
+	@SystemStub
+	private static EnvironmentVariables ENV_VARS;
+
 	@BeforeAll
 	static void startContainer() {
-		container = new OracleContainer();
+		if (runningOnMacArm64()) {
+			LOG.warn(() -> "You are running on Mac ARM64. If this test fails, make sure Colima is running prior " +
+					"to test invocation. See " + WIKI_LINK + " for details");
+			ENV_VARS.set("TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE", "/var/run/docker.sock");
+			ENV_VARS.set("DOCKER_HOST", String.format("unix://%s/.colima/docker.sock", System.getProperty("user.home")));
+		}
+		container = new OracleContainer(DockerImageName.parse("gvenzl/oracle-xe")
+				.withTag("18-slim-faststart"));
 		container.start();
+	}
+
+	private static boolean runningOnMacArm64() {
+		String osName = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
+		String osArchitecture = System.getProperty("os.arch").toLowerCase(Locale.ENGLISH);
+		return osName.contains("mac") && osArchitecture.equals("aarch64");
 	}
 }
