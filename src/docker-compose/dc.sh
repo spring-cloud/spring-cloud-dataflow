1d981f5399666d7c302b13cd1622635c679d19d7
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2023-12-04 12:13:12 +0200

    Updated dc.sh to detect the absence of `docker-compose` executable and use `docker compose`. Added output the show complete commandline before execution.

--- src/docker-compose/dc.sh
+++ src/docker-compose/dc.sh
@@ -6,12 +6,13 @@ fi
 SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 SCDIR=$(realpath $SCDIR)
 if [ "$1" == "" ]; then
-    echo "Usage is: "${BASH_SOURCE[0]}" <database> <broker> [options] [flags]"
+    echo "Usage is: "${BASH_SOURCE[0]}" <database> <broker> [options] [flags] [--no-wait]"
     echo "Where:"
     echo "    database: is one of mariadb, mysql or postgres"
     echo "    broker: is on of kafka or rabbitmq"
     echo "    options: one or more of ssl, zipkin, prometheus, influxdb, wavefront, ssl, dood, debug-dataflow, debug-skipper"
     echo "    flags: any docker-compose options"
+    echo "    --no-wait: Don't display command line and don't wait for input."
     echo "  This will invoke docker-compose with all the relevant files and provided options as well as the 'up' command"
     exit 1
 fi
@@ -19,6 +20,7 @@ BROKER=rabbitmq
 DATABASE=postgres
 ARGS=
 DC_OPTS=
+NO_WAIT=false
 while [ "$1" != "" ]; do
     case $1 in
     "rabbit" | "rabbitmq")
@@ -35,6 +37,9 @@ while [ "$1" != "" ]; do
         ;;
     "postgres" | "postgresql")
         DATABASE=postgres
+        ;;
+    "--no-wait")
+        NO_WAIT=true
         ;;
     *)
         if [ -f "$SCDIR/docker-compose-$1.yml" ]; then
@@ -65,4 +70,17 @@ fi
 if [ "$DC_OPTS" = "" ]; then
     DC_OPTS=up
 fi
-docker-compose -f "$SCDIR/docker-compose.yml" -f "$SCDIR/docker-compose-$BROKER.yml" -f "$SCDIR/docker-compose-$DATABASE.yml" $ARGS $DC_OPTS
+set +e
+docker-compose -v 2&> /dev/null
+RC=$?
+if ((RC==0)); then
+    DC="docker-compose"
+else
+    DC="docker compose"
+fi
+if [ "$NO_WAIT" == "false" ]; then
+    echo "Invoking:$DC -f $SCDIR/docker-compose.yml -f $SCDIR/docker-compose-$BROKER.yml -f $SCDIR/docker-compose-$DATABASE.yml $ARGS $DC_OPTS"
+    echo "Press any key to continue..."
+    read -s -n 1
+fi
+$DC -f "$SCDIR/docker-compose.yml" -f "$SCDIR/docker-compose-$BROKER.yml" -f "$SCDIR/docker-compose-$DATABASE.yml" $ARGS $DC_OPTS
