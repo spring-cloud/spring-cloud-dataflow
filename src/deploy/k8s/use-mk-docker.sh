2015b65a163abc6b80de3d39ecb108dd7e758654
Glenn Renfro <grenfro@vmware.com>
2024-01-02 16:42:39 -0500

    Script should not end terminal if not using bash
    If the user hit the exit 0 (when using source) and
    they did not use bash  terminal would exit.
    The user would not know why the terminal exited.
    
    Updated script to just check to see if there is bash available.
    
    Set namespace to default if not specified
    
    Add additional instructions when not running in bash
    
    Document the use of the --namespace key
    
    namespace is no longer positional but requires a --namespace or -ns key

--- src/deploy/k8s/use-mk-docker.sh
+++ src/deploy/k8s/use-mk-docker.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-(return 0 2>/dev/null) && sourced=1 || sourced=0
-if [ "$sourced" = "0" ]; then
-  echo "This script must be invoked using: source $0 $*"
-  exit 1
-fi
 if [ -z "$BASH_VERSION" ]; then
-    echo "This script requires Bash. Use: bash $0 $*"
-    exit 0
+    echo "This script requires Bash. For example:\n bash \n $0 $*"
+else
+    echo "Setting variables"
+    SCDIR=$(readlink -f "${BASH_SOURCE[0]}")
+    SCDIR=$(dirname "$SCDIR")
+    SCDIR=$(realpath $SCDIR)
+    source $SCDIR/use-mk.sh docker $*
 fi
-SCDIR=$(readlink -f "${BASH_SOURCE[0]}")
-SCDIR=$(dirname "$SCDIR")
-SCDIR=$(realpath $SCDIR)
-source $SCDIR/use-mk.sh docker $*
