6bfe953d0d6dfd3f9ba8bc544d593c2597088f40
Chris Bono <chris.bono@gmail.com>
2024-02-09 11:13:22 -0600

    Fix tests for rest-client (SCDF3 migration)
    This commit excludes DataSourceAutoConfiguration from the tests.

--- spring-cloud-dataflow-rest-client/src/test/java/org/springframework/cloud/dataflow/rest/client/DataFlowClientAutoConfigurationTests.java
+++ spring-cloud-dataflow-rest-client/src/test/java/org/springframework/cloud/dataflow/rest/client/DataFlowClientAutoConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2016-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import org.mockito.Mockito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.dataflow.rest.client.config.DataFlowClientProperties;
 import org.springframework.cloud.dataflow.rest.client.dsl.StreamBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -67,7 +68,7 @@ public class DataFlowClientAutoConfigurationTests {
 		applicationContext.close();
 	}
 
-	@SpringBootApplication
+	@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 	static class TestApplication {
 
 		@Bean
