24e9aa472fc6a0f76e714fcb6547314e288bca46
Glenn Renfro <grenfro@vmware.com>
2024-05-23 15:55:30 -0400

    All src/deploy scripts must be updated to the 2.11.4 release.
    This is a followup activity for the 2.11.3 release of Spring Cloud Data Flow
    This inclues the update of the pro to 1.6.3

--- src/deploy/images/pull-grafana.sh
+++ src/deploy/images/pull-grafana.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ "$DATAFLOW_VERSION" = "" ]; then
-  DATAFLOW_VERSION=2.11.3-SNAPSHOT
+  DATAFLOW_VERSION=2.11.4-SNAPSHOT
 fi
 docker pull "springcloud/spring-cloud-dataflow-grafana-prometheus:$DATAFLOW_VERSION"
--- src/deploy/images/pull-skipper.sh
+++ src/deploy/images/pull-skipper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$SKIPPER_VERSION" = "" ]; then
-  SKIPPER_VERSION=2.11.3-SNAPSHOT
+  SKIPPER_VERSION=2.11.4-SNAPSHOT
 fi
 
 docker pull "springcloud/spring-cloud-skipper-server:$SKIPPER_VERSION"
