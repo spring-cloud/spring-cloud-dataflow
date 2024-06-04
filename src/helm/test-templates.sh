198aa89277e8fff4e7a7a94fc567674d2db34de8
Corneil du Plessis <pcorneil@vmware.com>
2024-06-04 17:39:43 +0200

    Remove comments and improve verification of json response.

--- src/helm/test-templates.sh
+++ src/helm/test-templates.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 pushd "$SCDIR" > /dev/null || exit
-helm template --debug scdf/ --values scdf/test-values.yaml | tee test.yml
 helm lint --debug scdf/ --values scdf/test-values.yaml
+rm -rf ./output/*
+helm template --debug scdf/ --values scdf/test-values.yaml --output-dir ./output
 popd > /dev/null || exit
