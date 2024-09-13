93e00cdfaf53e71ae2ae3ce6c1cc7a263ba56808
Çağrı ÜZEL <cagriuzel@gmail.com>
2024-09-13 13:50:34 +0300

    Update CustomAuthoritiesOpaqueTokenIntrospector.java
    fix build

--- spring-cloud-common-security-config/spring-cloud-common-security-config-web/src/main/java/org/springframework/cloud/common/security/support/CustomAuthoritiesOpaqueTokenIntrospector.java
+++ spring-cloud-common-security-config/spring-cloud-common-security-config-web/src/main/java/org/springframework/cloud/common/security/support/CustomAuthoritiesOpaqueTokenIntrospector.java
@@ -20,6 +20,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.ArrayList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
