a99631a02ed4d9f01de787656607186da8c82a06
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-10-02 16:22:50 +0200

    [CI] Update maven settings and use of wrapper.

--- src/deploy/images/build-grafana-image.sh
+++ src/deploy/images/build-grafana-image.sh
@@ -7,7 +7,7 @@ SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 PROJECT_DIR=$(realpath $SCDIR/../../..)
 pushd $PROJECT_DIR || exit
     if [ "$DATAFLOW_VERSION" = "" ]; then
-        SCDF_VER=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+        SCDF_VER=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
     else
         SCDF_VER=$DATAFLOW_VERSION
     fi
