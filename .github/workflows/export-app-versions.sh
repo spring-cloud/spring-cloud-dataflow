65fa6bc67de7a21b92620080757b444b8e4e80d9
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-10-03 11:01:07 +0200

    [CI] Fix help:evalutate when using wrapper.

--- .github/workflows/export-app-versions.sh
+++ .github/workflows/export-app-versions.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set +e
-./mvnw help:evaluate -Dexpression=project.version
-DATAFLOW_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
+./mvnw --version
+DATAFLOW_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -o -q -DforceStdout)
 RC=$?
 if ((RC!=0)); then
     echo "DATAFLOW_VERSION=$DATAFLOW_VERSION"
     exit $RC
 fi
 echo "DATAFLOW_VERSION=$DATAFLOW_VERSION"
-SKIPPER_VERSION=$(./mvnw help:evaluate -Dexpression=spring-cloud-skipper.version  -pl spring-cloud-dataflow-parent -q -DforceStdout)
+SKIPPER_VERSION=$(./mvnw help:evaluate -o -Dexpression=spring-cloud-skipper.version  -pl spring-cloud-dataflow-parent -q -DforceStdout)
 if [[ "$SKIPPER_VERSION" = *"ERROR"* ]]; then
-    SKIPPER_VERSION=$(./mvnw help:evaluate -Dexpression=spring-cloud-skipper.version -q -DforceStdout)
+    SKIPPER_VERSION=$(./mvnw help:evaluate -o -Dexpression=spring-cloud-skipper.version -q -DforceStdout)
 fi
 RC=$?
 if ((RC!=0)); then
