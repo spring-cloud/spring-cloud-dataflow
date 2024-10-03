a49589f410844cc9982f7325a9ccce31d63de68b
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-10-03 15:22:12 +0200

    [CI] fix set-package-version.sh

--- spring-cloud-dataflow-package/set-package-version.sh
+++ spring-cloud-dataflow-package/set-package-version.sh
@@ -4,17 +4,18 @@ if [ -z "$BASH_VERSION" ]; then
     exit 0
 fi
 SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-MVNW=$SCDIR/../mvnw
-MVND=$(dirname $MVNW)
+ROOT=$(realpath $SCDIR/..)
 if [ "$PACKAGE_VERSION" = "" ]; then
-    $MVNW help:evaluate -s $MVND/.settings.xml -Dexpression=project.version > /dev/null
-    PACKAGE_VERSION=$($MVNW help:evaluate -Dexpression=project.version -o -q -DforceStdout)
+    pushd $ROOT > /dev/null
+    ./mvnw help:evaluate -s .settings.xml -Dexpression=project.version > /dev/null
+    PACKAGE_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -o -q -DforceStdout)
+    popd > /dev/null
 fi
 echo "PACKAGE_VERSION=$PACKAGE_VERSION"
 if [[ "$PACKAGE_VERSION" != *"SNAPSHOT"* ]]; then
-    yq '.default.version="release"' -i "$SCDIR/../src/deploy/versions.yaml"
+    yq '.default.version="release"' -i "$ROOT/src/deploy/versions.yaml"
     echo "Setting default.version=release, default.package-version=$PACKAGE_VERSION"
-    yq ".default.package-version=\"$PACKAGE_VERSION\"" -i "$SCDIR/../src/deploy/versions.yaml"
+    yq ".default.package-version=\"$PACKAGE_VERSION\"" -i "$ROOT/src/deploy/versions.yaml"
     echo "Setting scdf-type.oss.release=$PACKAGE_VERSION"
-    yq ".scdf-type.oss.release=\"$PACKAGE_VERSION\"" -i "$SCDIR/../src/deploy/versions.yaml"
+    yq ".scdf-type.oss.release=\"$PACKAGE_VERSION\"" -i "$ROOT/src/deploy/versions.yaml"
 fi
