e56953c11cc639fa3d4d3ddbfbce326b39a3d58b
Glenn Renfro <grenfro@vmware.com>
2024-10-04 09:50:27 -0400

    Update maven-surefire-report-plugin to 3.5.0
    Replace the hardcoded version with a property in each dependency,
    Set the property in the spring-cloud-dataflow parent
    
    You will also notice I removed a unused import in a test.   Can remove if it needs to be put in another PR

--- spring-cloud-dataflow-composed-task-runner/src/test/java/org/springframework/cloud/dataflow/composedtaskrunner/ComposedTaskRunnerConfigurationWithVersionPropertiesTests.java
+++ spring-cloud-dataflow-composed-task-runner/src/test/java/org/springframework/cloud/dataflow/composedtaskrunner/ComposedTaskRunnerConfigurationWithVersionPropertiesTests.java
@@ -21,7 +21,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.batch.core.Job;
