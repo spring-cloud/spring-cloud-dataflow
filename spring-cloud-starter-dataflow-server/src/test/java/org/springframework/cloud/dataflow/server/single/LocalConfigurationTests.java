4cf53e137713ab66fab52d0ab59f3e472cece365
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-10-14 16:39:30 +0200

    DataflowTaskExecutionQueryDao was previously configured in module now removed.

--- spring-cloud-starter-dataflow-server/src/test/java/org/springframework/cloud/dataflow/server/single/LocalConfigurationTests.java
+++ spring-cloud-starter-dataflow-server/src/test/java/org/springframework/cloud/dataflow/server/single/LocalConfigurationTests.java
@@ -19,7 +19,6 @@ package org.springframework.cloud.dataflow.server.single;
 import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
