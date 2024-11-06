7d56831526e83c5f017f2b755ab531d2016ef0b7
Glenn Renfro <grenfro@vmware.com>
2024-11-06 07:51:35 -0500

    Remove commented out code.
    This code was removed in the 2.11.x line.
    
    Signed-off-by: Glenn Renfro <grenfro@vmware.com>

--- spring-cloud-dataflow-common/spring-cloud-dataflow-common-test-docker/src/test/java/org/springframework/cloud/dataflow/common/test/docker/compose/logging/LogDirectoryTest.java
+++ spring-cloud-dataflow-common/spring-cloud-dataflow-common-test-docker/src/test/java/org/springframework/cloud/dataflow/common/test/docker/compose/logging/LogDirectoryTest.java
@@ -21,37 +21,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LogDirectoryTest {
 
-//    @Rule
-//    public final EnvironmentVariables variablesRule = new EnvironmentVariables();
-
     @Test
     public void gradleDockerLogsDirectory_should_use_class_simple_name() {
         String directory = LogDirectory.gradleDockerLogsDirectory(SomeTestClass.class);
 		assertThat(directory).isEqualTo("build/dockerLogs/SomeTestClass");
     }
-
-//    @Test
-//    public void circleAwareLogDirectory_should_match_gradleDockerLogsDirectory_by_default() {
-//        variablesRule.set("CIRCLE_ARTIFACTS", null);
-//        String directory = LogDirectory.circleAwareLogDirectory(SomeTestClass.class);
-//        assertThat(directory, is("build/dockerLogs/SomeTestClass"));
-//    }
-//
-//    @Test
-//    public void circleAwareLogDirectory_should_use_circle_environment_variable_if_available() {
-//        variablesRule.set("CIRCLE_ARTIFACTS", "/tmp/circle-artifacts.g4DjuuD");
-//
-//        String directory = LogDirectory.circleAwareLogDirectory(SomeTestClass.class);
-//        assertThat(directory, is("/tmp/circle-artifacts.g4DjuuD/dockerLogs/SomeTestClass"));
-//    }
-//
-//    @Test
-//    public void circleAwareLogDirectory_should_append_logDirectoryName_to_path() {
-//        variablesRule.set("CIRCLE_ARTIFACTS", "/tmp/circle-artifacts.123456");
-//
-//        String directory = LogDirectory.circleAwareLogDirectory("some-path");
-//        assertThat(directory, is("/tmp/circle-artifacts.123456/dockerLogs/some-path"));
-//    }
 
     private static class SomeTestClass {}
 }
